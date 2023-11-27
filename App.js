2f8ae5b76c2e8459f94c729919db358b92f40c4f
Manu G <manusvathgurudath@gmail.com>
2023-11-27 00:31:39 -0800

    Add dependencies and update UI

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView, View, Pressable } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
+
+export const AuthContext = React.createContext({
+  token: null,
+  setToken: () => {},
+});
+
+function IntroScreen() {
+  return (
+    <>
+      <Text>Welcome to Dysperse</Text>
+      {/* Add 2 buttons */}
+      <Pressable
+        onPress={() => {}}
+        style={{
+          backgroundColor: "blue",
+          padding: 10,
+          borderRadius: 5,
+        }}
+      >
+        <Text>Sign in</Text>
+      </Pressable>
+      <Pressable onPress={() => {}}>
+        <Text>Sign Up</Text>
+      </Pressable>
+    </>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+      >
+        <StatusBar style="auto" />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Authentication"
+            options={{
+              header: () => null,
+            }}
+            component={IntroScreen}
+          />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
