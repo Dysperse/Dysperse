66676a541cbfa59a0c91201b15596d95d11e0afe
Manu G <manusvathgurudath@gmail.com>
2023-11-27 01:05:39 -0800

    yessir

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, View, Pressable } from "react-native";
+import { StyleSheet, SafeAreaView, View, Pressable } from "react-native";
+import { Text } from "react-native-paper";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { blue } from "./themes";
+import { PaperProvider } from "react-native-paper";
+import { Button } from "react-native-paper";
+
 const Stack = createStackNavigator();
 
 export const AuthContext = React.createContext({
@@ -13,45 +17,49 @@ export const AuthContext = React.createContext({
 
 function IntroScreen() {
   return (
-    <>
-      <Text>Welcome to Dysperse!</Text>
-      {/* Add 2 buttons */}
-      <Pressable
-        onPress={() => {}}
-        style={{
-          backgroundColor: blue["blue9"],
-          padding: 10,
-          borderRadius: 5,
-        }}
-      >
-        <Text>Sign in</Text>
-      </Pressable>
-      <Pressable onPress={() => {}}>
-        <Text>Sign Up</Text>
-      </Pressable>
-    </>
+    <View
+      style={{
+        padding: 20,
+        height: "100%",
+        justifyContent: "flex-end",
+        gap: 10,
+      }}
+    >
+      <Text variant="displayLarge">Welcome to Dysperse.</Text>
+      <Text variant="headlineSmall">
+        We're here to redefine the standard for productivity.
+      </Text>
+      <Button mode="contained" onPress={() => console.log("Pressed")}>
+        Log in
+      </Button>
+      <Button mode="text" onPress={() => console.log("Pressed")}>
+        New to Dysperse? Sign up
+      </Button>
+    </View>
   );
 }
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView
-        style={{
-          flex: 1,
-        }}
-      >
-        <StatusBar style="auto" />
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Authentication"
-            options={{
-              header: () => null,
-            }}
-            component={IntroScreen}
-          />
-        </Stack.Navigator>
-      </SafeAreaView>
+      <PaperProvider>
+        <SafeAreaView
+          style={{
+            flex: 1,
+          }}
+        >
+          <StatusBar style="auto" />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Authentication"
+              options={{
+                header: () => null,
+              }}
+              component={IntroScreen}
+            />
+          </Stack.Navigator>
+        </SafeAreaView>
+      </PaperProvider>
     </NavigationContainer>
   );
 }
