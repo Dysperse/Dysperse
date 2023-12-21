c7277ac7d3cab96227a7341f855901b70b723896
Manu G <manusvathgurudath@gmail.com>
2023-12-21 08:54:11 +0000

    W

--- app/(app)/[tab]/all/[type].tsx
+++ app/(app)/[tab]/all/[type].tsx
@@ -2,6 +2,7 @@ import { ContentWrapper } from "@/components/layout/content";
 import Divider from "@/ui/Divider";
 import Text from "@/ui/Text";
 import { useLocalSearchParams } from "expo-router";
+import { ScrollView } from "react-native";
 import useSWR from "swr";
 
 export default function Page() {
@@ -10,9 +11,11 @@ export default function Page() {
 
   return (
     <ContentWrapper>
-      <Text>{JSON.stringify(params, null, 2)}</Text>
-      <Divider />
-      <Text>{JSON.stringify(data, null, 2)}</Text>
+      <ScrollView>
+        <Text>{JSON.stringify(params, null, 2)}</Text>
+        <Divider />
+        <Text>{JSON.stringify(data, null, 2)}</Text>
+      </ScrollView>
     </ContentWrapper>
   );
 }
