bba3bd4e3878a8556ac6baae759246519f2298ea
Manu G <manusvathgurudath@gmail.com>
2023-12-28 20:06:30 +0000

    Update index.html and theme-provider.tsx

--- ui/color/theme-provider.tsx
+++ ui/color/theme-provider.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect } from "react";
+import { Platform } from "react-native";
 
 const ColorThemeContext = createContext(null);
 export const useColorTheme = () => useContext(ColorThemeContext);
@@ -10,6 +11,14 @@ export const ColorThemeProvider = ({
   theme: Record<string, string>;
   children: React.ReactNode;
 }) => {
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      const variables = Object.entries(theme).map(
+        ([key, value]) => `--theme-${key}: ${value};`
+      );
+      document.body.style.cssText = variables.join(" ");
+    }
+  });
   return (
     <ColorThemeContext.Provider value={theme}>
       {children}
