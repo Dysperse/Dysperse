90057b6f436739dbc1739f9434e86a7364fed81c
Manu G <manusvathgurudath@gmail.com>
2024-01-05 21:40:25 +0000

    n

---
+++ app/(app)/[tab]/perspectives/[type].tsx
@@ -0,0 +1,10 @@
+import { ContentWrapper } from "@/components/layout/content";
+import Text from "@/ui/Text";
+
+export default function Page() {
+  return (
+    <ContentWrapper>
+      <Text>Todo: Stream</Text>
+    </ContentWrapper>
+  );
+}
