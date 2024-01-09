d88baca07cf66c9fd034ee6580fa514b527318e6
Manu G <manusvathgurudath@gmail.com>
2024-01-09 09:07:38 +0000

    W

--- components/perspectives/agenda/Column.tsx
+++ components/perspectives/agenda/Column.tsx
@@ -59,7 +59,7 @@ const PerspectivesEmptyComponent = memo(function PerspectivesEmptyComponent() {
     ["26c5", "Own your day!", "Effort = Results"],
     ["1f44a", "You're unstoppable!", "Quick stretch or snack"],
     ["1f5ff", "Crushing it!", "No task is too big"],
-    ["1f985", "Look at yourself", "Aren't you amazing!?"],
+    ["1f985", "Look at yourself", "You're beautiful."],
   ];
 
   const message = messages[Math.floor(Math.random() * messages.length)];
