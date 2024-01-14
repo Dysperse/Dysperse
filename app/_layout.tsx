256e42981782b5121409792541309470b844c212
Manu G <manusvathgurudath@gmail.com>
2024-01-13 16:57:55 -0800

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -46,6 +46,7 @@ Sentry.init({
   enableInExpoDevelopment: false,
   enableAutoSessionTracking: true,
   debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  tracesSampleRate: 1.0,
 });
 
 SplashScreen.preventAutoHideAsync();
