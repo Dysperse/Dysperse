b8b62ccbcbe4008303d9edfad696d123b2d61ced
Manu G <manusvathgurudath@gmail.com>
2024-02-25 11:22:35 -0800

    W

--- app/(app)/everything.tsx
+++ app/(app)/everything.tsx
@@ -1,6 +1,7 @@
 import { ContentWrapper } from "@/components/layout/content";
 import { ButtonGroup } from "@/ui/ButtonGroup";
 import { useColorTheme } from "@/ui/color/theme-provider";
+import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
 import { View } from "react-native";
 
@@ -9,27 +10,45 @@ export default function Page() {
   const [view, setView] = useState<"labels" | "collections">("labels");
   return (
     <ContentWrapper>
-      <ButtonGroup
-        options={[
-          { label: "Labels", value: "labels" },
-          { label: "Collections", value: "collections" },
-        ]}
-        state={[view, setView]}
-        buttonStyle={{ borderBottomWidth: 0 }}
-        containerStyle={{ width: 300 }}
-        scrollContainerStyle={{ width: "100%" }}
-        activeComponent={
-          <View
-            style={{
-              height: 4,
-              width: 10,
-              borderRadius: 99,
-              backgroundColor: theme[9],
-              margin: "auto",
-            }}
-          />
-        }
-      />
+      <LinearGradient
+        colors={[theme[2], theme[3]]}
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: theme[5],
+        }}
+      >
+        <ButtonGroup
+          options={[
+            { label: "Labels", value: "labels" },
+            { label: "Collections", value: "collections" },
+          ]}
+          state={[view, setView]}
+          buttonStyle={{ borderBottomWidth: 0 }}
+          buttonTextStyle={{
+            color: theme[9],
+            fontFamily: "body_400",
+            paddingHorizontal: 0,
+          }}
+          selectedButtonTextStyle={{ color: theme[11], fontFamily: "body_800" }}
+          containerStyle={{
+            width: 200,
+            marginHorizontal: "auto",
+            marginVertical: 10,
+          }}
+          scrollContainerStyle={{ width: "100%" }}
+          activeComponent={
+            <View
+              style={{
+                height: 4,
+                width: 10,
+                borderRadius: 99,
+                backgroundColor: theme[9],
+                margin: "auto",
+              }}
+            />
+          }
+        />
+      </LinearGradient>
     </ContentWrapper>
   );
 }
