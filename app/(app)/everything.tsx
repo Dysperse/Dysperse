637a623a89a5c04f3edca76c581b5a393697aa13
Manu G <manusvathgurudath@gmail.com>
2024-02-25 13:06:31 -0800

    W

--- app/(app)/everything.tsx
+++ app/(app)/everything.tsx
@@ -1,31 +1,59 @@
 import { ContentWrapper } from "@/components/layout/content";
 import { ButtonGroup } from "@/ui/ButtonGroup";
-import Text from "@/ui/Text";
+import Emoji from "@/ui/Emoji";
+import Icon from "@/ui/Icon";
+import { ListItemButton } from "@/ui/ListItemButton";
+import ListItemText from "@/ui/ListItemText";
+import Spinner from "@/ui/Spinner";
 import { useColorTheme } from "@/ui/color/theme-provider";
+import { FlashList } from "@shopify/flash-list";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
 import { View } from "react-native";
 import useSWR from "swr";
 
 const Labels = () => {
+  const [selectedLabel, setSelectedLabel] = useState<number | null>(null);
+
   const { data, mutate, error } = useSWR(["space/labels"]);
 
+  const d = data?.map((l) => ({ ...l, selected: l.id === selectedLabel }));
+  const selectedLabelData =
+    selectedLabel && d.find((i) => i.id === selectedLabelData);
   return (
-    <>
+    <View style={{ flexDirection: "row", flex: 1 }}>
       {Array.isArray(data) ? (
-        <View>
-          {data.map((label) => (
-            <View key={label.id}>
-              <Text>{label.name}</Text>
-            </View>
-          ))}
-        </View>
+        <>
+          <View style={{ flex: 1 }}>
+            <FlashList
+              estimatedItemSize={60}
+              data={d}
+              contentContainerStyle={{ padding: 20 }}
+              showsVerticalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <ListItemButton
+                  style={{ height: 60 }}
+                  onPress={() => setSelectedLabel(item.id)}
+                >
+                  <Emoji emoji={item.emoji} size={20} />
+                  <ListItemText
+                    primary={item.name}
+                    secondary={`${item._count.entities} item${
+                      item._count.entities.length !== 1 ? "s" : ""
+                    }`}
+                  />
+                  {selectedLabel === item.id && <Icon>check</Icon>}
+                </ListItemButton>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+            />
+          </View>
+          <View style={{ flex: 2 }}>{selectedLabelData}</View>
+        </>
       ) : (
-        <View>
-          <Text>Loading...</Text>
-        </View>
+        <Spinner />
       )}
-    </>
+    </View>
   );
 };
 
