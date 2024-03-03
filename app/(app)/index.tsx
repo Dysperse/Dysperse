98a5298d053fa018c7b89352f01f107d7ee2370c
Manu G <manusvathgurudath@gmail.com>
2024-03-03 00:03:52 -0800

    W

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -112,7 +112,7 @@ function PlanDayPrompt() {
 
 export const getProfileLastActiveRelativeTime = (time) => {
   const t = dayjs(time).fromNow(true);
-  return t.includes("few") || dayjs().diff(time, "minute") < 5
+  return t.includes("few") || dayjs().diff(time, "minute") < 3
     ? "NOW"
     : t.split(" ")[0].replace("a", "1") + t.split(" ")?.[1]?.[0].toUpperCase();
 };
