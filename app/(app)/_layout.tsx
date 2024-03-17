333360ed2f66b2e3df4c8811f1e1c618b1a93ad7
Manu G <manusvathgurudath@gmail.com>
2024-03-17 00:03:02 -0700

    W

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -10,7 +10,9 @@ import {
   useStorageContext,
 } from "@/context/storageContext";
 import { useUser } from "@/context/useUser";
+import { sendApiRequest } from "@/helpers/api";
 import { useResponsiveBreakpoints } from "@/helpers/useResponsiveBreakpoints";
+import { Button } from "@/ui/Button";
 import Icon from "@/ui/Icon";
 import Text from "@/ui/Text";
 import { addHslAlpha, useColor, useDarkMode } from "@/ui/color";
@@ -30,6 +32,7 @@ import isoWeek from "dayjs/plugin/isoWeek";
 import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
+import { BlurView } from "expo-blur";
 import * as NavigationBar from "expo-navigation-bar";
 import { Redirect } from "expo-router";
 import { useEffect } from "react";
@@ -46,7 +49,9 @@ import {
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
+  ScrollView,
 } from "react-native-gesture-handler";
+import Markdown from "react-native-markdown-display";
 import { MenuProvider } from "react-native-popup-menu";
 import Animated, {
   interpolate,
@@ -56,6 +61,7 @@ import Animated, {
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 import "react-native-url-polyfill/auto";
+import useSWR from "swr";
 import { useSidebarContext } from "../../components/layout/sidebar/context";
 
 dayjs.extend(customParseFormat);
@@ -199,6 +205,116 @@ const ComingSoonScreen = () => {
   return null;
 };
 
+const ReleaseModal = () => {
+  const { session, mutate, sessionToken } = useUser();
+  const theme = useColorTheme();
+  const { data } = useSWR("releases", {
+    fetcher: () =>
+      fetch(
+        `https://api.github.com/repos/dysperse/dysperse/releases?per_page=1`
+      ).then((res) => res.json()),
+  });
+
+  const handleDone = () => {
+    mutate(
+      (d) => {
+        return {
+          ...d,
+          user: { ...d.user, lastReleaseVersionViewed: data?.[0]?.id },
+        };
+      },
+      {
+        revalidate: false,
+      }
+    );
+    sendApiRequest(
+      sessionToken,
+      "PUT",
+      "user/account",
+      {},
+      { body: JSON.stringify({ lastReleaseVersionViewed: data?.[0]?.id }) }
+    );
+  };
+
+  return (
+    data &&
+    data?.[0]?.id &&
+    session.user.lastReleaseVersionViewed !== data?.[0]?.id && (
+      <Portal>
+        <BlurView
+          tint="systemUltraThinMaterialLight"
+          intensity={50}
+          style={{
+            zIndex: 99,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.5)",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            icon="done"
+            text="Got it!"
+            variant="filled"
+            large
+            style={({ pressed, hovered }) => ({
+              position: "absolute",
+              top: 20,
+              right: 20,
+              zIndex: 99,
+              backgroundColor: theme[pressed ? 7 : hovered ? 6 : 5],
+            })}
+            onPress={handleDone}
+          />
+          <ScrollView
+            style={{
+              width: "100%",
+              padding: 20,
+              maxWidth: 700,
+              paddingVertical: 100,
+            }}
+            showsVerticalScrollIndicator={false}
+          >
+            <Text style={{ fontSize: 50, color: theme[11] }} weight={900}>
+              What's new!?
+            </Text>
+            <Text
+              style={{ fontSize: 33, color: theme[11], opacity: 0.6 }}
+              weight={500}
+            >
+              {data?.[0]?.name} &bull;{" "}
+              {dayjs(data?.[0]?.published_at).fromNow()}
+            </Text>
+            <Markdown
+              style={{
+                body: {
+                  color: theme[11],
+                  fontFamily: "body_400",
+                  fontSize: 15,
+                },
+                heading1: { fontFamily: "body_800", fontSize: 30 },
+                heading2: { fontFamily: "body_800", fontSize: 27 },
+                heading3: {
+                  fontFamily: "body_800",
+                  marginTop: 20,
+                  fontSize: 24,
+                },
+              }}
+            >
+              {data?.[0]?.body?.split("<!--dysperse-changelog-end-->")?.[0] ||
+                ""}
+            </Markdown>
+          </ScrollView>
+        </BlurView>
+      </Portal>
+    )
+  );
+};
+
 const LoadingErrors = () => {
   const red = useColor("red");
   const theme = useColorTheme();
@@ -333,6 +449,7 @@ export default function AppLayout() {
                             breakpoints.md ? { flex: 1 } : { width: "100%" },
                           ]}
                         >
+                          <ReleaseModal />
                           <LoadingErrors />
                           <Drawer
                             open={isOpen}
