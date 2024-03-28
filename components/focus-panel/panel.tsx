b0aa335b74dbb3dc89b376b6754cd393d20c8966
Manu G <manusvathgurudath@gmail.com>
2024-03-27 23:54:14 -0700

    W

--- components/focus-panel/panel.tsx
+++ components/focus-panel/panel.tsx
@@ -1,6 +1,9 @@
+import { useUser } from "@/context/useUser";
+import { sendApiRequest } from "@/helpers/api";
 import { useHotkeys } from "@/helpers/useHotKeys";
 import { useResponsiveBreakpoints } from "@/helpers/useResponsiveBreakpoints";
 import Alert from "@/ui/Alert";
+import { Avatar, ProfilePicture } from "@/ui/Avatar";
 import { Button, ButtonText } from "@/ui/Button";
 import Chip from "@/ui/Chip";
 import Emoji from "@/ui/Emoji";
@@ -10,14 +13,18 @@ import IconButton from "@/ui/IconButton";
 import MenuPopover, { MenuItem } from "@/ui/MenuPopover";
 import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
+import TextField from "@/ui/TextArea";
 import { useColor } from "@/ui/color";
 import { ColorThemeProvider, useColorTheme } from "@/ui/color/theme-provider";
+import Logo from "@/ui/logo";
 import dayjs from "dayjs";
 import { useKeepAwake } from "expo-keep-awake";
 import { usePathname } from "expo-router";
 import { Fragment, memo, useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { Platform, Pressable, View, useWindowDimensions } from "react-native";
 import {
+  FlatList,
   Gesture,
   GestureDetector,
   ScrollView,
@@ -29,6 +36,7 @@ import Animated, {
 } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Path, Svg } from "react-native-svg";
+import Toast from "react-native-toast-message";
 import useSWR from "swr";
 import { Entity } from "../collections/entity";
 import {
@@ -256,15 +264,171 @@ const UpNext = () => {
 };
 
 const Assistant = () => {
+  const { session, sessionToken } = useUser();
+  const theme = useColorTheme();
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  const sendMessage = async ({ message }) => {
+    try {
+      setLoading(true);
+
+      setMessages((m) => [
+        ...m,
+        {
+          role: "user",
+          message,
+        },
+      ]);
+
+      reset();
+
+      const data = await sendApiRequest(sessionToken, "POST", "ai/assistant", {
+        input: message,
+      });
+      setMessages((m) => [
+        ...m,
+        {
+          role: "bot",
+          message: data.response,
+        },
+      ]);
+    } catch (error) {
+      console.error(error);
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: "An error occurred while sending the message",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <View>
       <Text variant="eyebrow">Assistant</Text>
-      <Alert
-        style={{ marginTop: 10 }}
-        title="Coming soon"
-        emoji="1f6a7"
-        subtitle="We're working on this widget - stay tuned!"
-      />
+      <View
+        style={{
+          backgroundColor: theme[2],
+          borderWidth: 1,
+          borderColor: theme[5],
+          marginVertical: 10,
+          borderRadius: 20,
+          overflow: "hidden",
+        }}
+      >
+        <FlatList
+          inverted
+          contentContainerStyle={{
+            padding: 10,
+            borderRadius: 10,
+            marginTop: 10,
+            height: 300,
+            gap: 20,
+          }}
+          data={messages.slice().reverse()}
+          renderItem={({ item, index }) => (
+            <View
+              style={[
+                {
+                  flexDirection: item.role === "user" ? "row-reverse" : "row",
+                  gap: 10,
+                  alignItems: item.role === "bot" ? "flex-end" : "flex-start",
+                },
+                index === messages.length - 1 && { marginBottom: 20 },
+              ]}
+            >
+              <View>
+                {item.role === "bot" ? (
+                  <Avatar size={30}>
+                    <Logo size={25} />
+                  </Avatar>
+                ) : (
+                  <ProfilePicture
+                    name={session.user.profile.name}
+                    image={session.user.profile.picture}
+                    size={30}
+                  />
+                )}
+              </View>
+              <View
+                style={[
+                  {
+                    padding: 10,
+                    paddingHorizontal: 20,
+                    borderRadius: 20,
+                    maxWidth: 200,
+                    height: "auto",
+                  },
+                  item.role === "user" && {
+                    marginLeft: "auto",
+                    backgroundColor: theme[9],
+                  },
+                  item.role === "bot" && {
+                    marginRight: "auto",
+                    backgroundColor: theme[4],
+                  },
+                ]}
+              >
+                <Text>{item.message}</Text>
+              </View>
+            </View>
+          )}
+          ListHeaderComponent={
+            loading && (
+              <View
+                style={{ alignItems: "center", gap: 10, flexDirection: "row" }}
+              >
+                <Avatar size={30}>
+                  <Logo size={25} />
+                </Avatar>
+                <Spinner />
+              </View>
+            )
+          }
+          keyExtractor={(item, i) => i.toString()}
+        />
+        <Controller
+          name="message"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <View
+              style={{
+                borderWidth: 1,
+                borderColor: theme[4],
+                flexDirection: "row",
+                backgroundColor: theme[3],
+              }}
+            >
+              <TextField
+                placeholder="Message"
+                value={value}
+                onChangeText={onChange}
+                style={{
+                  flex: 1,
+                  paddingHorizontal: 15,
+                  height: 45,
+                  shadowRadius: 0,
+                }}
+                blurOnSubmit={false}
+                onSubmitEditing={handleSubmit(sendMessage)}
+              />
+              <IconButton
+                icon="send"
+                size={45}
+                onPress={handleSubmit(sendMessage)}
+              />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 };
