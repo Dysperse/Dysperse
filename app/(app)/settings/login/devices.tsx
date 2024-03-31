ace5873008b47324d3da3575d1b256e61aada7da
Manu G <manusvathgurudath@gmail.com>
2024-03-30 22:08:38 -0700

    W

--- app/(app)/settings/login/devices.tsx
+++ app/(app)/settings/login/devices.tsx
@@ -2,19 +2,21 @@ import { SettingsLayout } from "@/components/settings/layout";
 import { settingStyles } from "@/components/settings/settingsStyles";
 import { useSession } from "@/context/AuthProvider";
 import { sendApiRequest } from "@/helpers/api";
+import { useResponsiveBreakpoints } from "@/helpers/useResponsiveBreakpoints";
+import { Avatar } from "@/ui/Avatar";
 import { Button, ButtonText } from "@/ui/Button";
 import ConfirmationModal from "@/ui/ConfirmationModal";
 import ErrorAlert from "@/ui/Error";
 import Icon from "@/ui/Icon";
-import { ListItemButton } from "@/ui/ListItemButton";
 import ListItemText from "@/ui/ListItemText";
 import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
+import { useDarkMode } from "@/ui/color";
 import { useColorTheme } from "@/ui/color/theme-provider";
 import { FlashList } from "@shopify/flash-list";
 import dayjs from "dayjs";
 import { Image } from "expo-image";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import Toast from "react-native-toast-message";
 import useSWR, { KeyedMutator } from "swr";
 
@@ -54,23 +56,57 @@ export function SessionCard({
   ];
 
   const { data, error } = useSWR(["user/sessions/device", { ip: session.ip }]);
+  const dark = useDarkMode();
 
+  const breakpoints = useResponsiveBreakpoints();
   return (
-    <View style={{ height: 150 }}>
-      <ListItemButton
-        variant={isCurrentDevice ? "filled" : "outlined"}
-        disabled
-      >
-        <Icon>{icons[session.deviceType || 0]}</Icon>
-        <ListItemText
-          truncate
-          primary={session.name || type[session.deviceType] || "Unknown device"}
-          secondary={`${session.ip} • ${dayjs(session.timestamp).fromNow()}`}
-        />
-        <Image
-          source={{ uri: data?.preview }}
-          style={{ width: 300, height: 300, borderRadius: 25 }}
-        />
+    <View
+      style={[
+        !breakpoints.md && { padding: 0 },
+        {
+          marginBottom: 15,
+          backgroundColor: theme[isCurrentDevice ? 3 : 0],
+          padding: 10,
+          borderRadius: breakpoints.md ? 25 : 25,
+          borderWidth: 1,
+          borderColor: theme[5],
+          gap: 20,
+          flexDirection: breakpoints.md ? "row" : "column-reverse",
+        },
+      ]}
+    >
+      <View style={{ flex: 1, gap: 10, justifyContent: "center" }}>
+        <View>
+          {data?.city && (
+            <View style={{ padding: 10 }}>
+              <Text variant="eyebrow">
+                {dayjs(session.timestamp).fromNow()}
+              </Text>
+              <Text style={[{ fontSize: 30 }]}>
+                {data?.city?.names?.en}, {data?.subdivisions?.[0]?.names?.en}
+              </Text>
+            </View>
+          )}
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            padding: 10,
+            marginTop: -20,
+            gap: 10,
+          }}
+        >
+          <Avatar size={40}>
+            <Icon>{icons[session.deviceType] || icons[0]}</Icon>
+          </Avatar>
+          <ListItemText
+            primary={
+              session.name || type[session.deviceType] || "Unknown device"
+            }
+            secondary={session.ip}
+          />
+        </View>
         <ConfirmationModal
           title="Sign out?"
           secondary="You'll need to log in again on this device."
@@ -93,7 +129,40 @@ export function SessionCard({
             </ButtonText>
           </Button>
         </ConfirmationModal>
-      </ListItemButton>
+      </View>
+      {data?.preview ? (
+        <Image
+          source={{ uri: data?.preview }}
+          style={{
+            ...(Platform.OS === "web" &&
+              dark && {
+                filter: "invert(5) brightness(2) contrast(0.8)",
+              }),
+            width: 300,
+            height: 300,
+            borderRadius: 25,
+            maxWidth: "100%",
+            aspectRatio: "1/1",
+          }}
+        />
+      ) : (
+        <View
+          style={{
+            width: 300,
+            height: 300,
+            borderRadius: 25,
+            backgroundColor: theme[4],
+            justifyContent: "center",
+            alignItems: "center",
+            maxWidth: "100%",
+            aspectRatio: "1/1",
+          }}
+        >
+          <Text style={{ opacity: 0.6, textAlign: "center" }}>
+            No preview available
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -121,7 +190,7 @@ export default function Page() {
     <SettingsLayout noScroll>
       {Array.isArray(data) ? (
         <FlashList
-          estimatedItemSize={85}
+          estimatedItemSize={330}
           data={data.sort((a) => (a.id === session ? -1 : 0))}
           ListHeaderComponent={() => (
             <View style={{ marginBottom: 20, paddingTop: 50 }}>
