df34d4ee32db6afaaeb4b589f2c88b0be5581373
Manu G <manusvathgurudath@gmail.com>
2024-04-02 11:08:46 -0700

    ??

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,8 @@
 module.exports = {
-	globDirectory: 'dist/',
-	globPatterns: [
-		'**/*.{js,ttf,ico,html,json,css}'
-	],
-	swDest: 'dist/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: "dist/",
+  globPatterns: ["**/*.{js,ttf,ico,html,json,css}"],
+  swDest: "dist/sw.js",
+  // 2 gigabytes
+  maximumFileSizeToCacheInBytes: 2 * 1024 * 1024 * 1024,
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
