f846196d2b886acffe8e6be74a99055d1511b111
Manu G <manusvathgurudath@gmail.com>
2024-04-08 01:05:36 -0700

    WORK

--- electron/src/main.js
+++ electron/src/main.js
@@ -1,21 +1,6 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, shell, Tray, Menu } = require("electron");
 const path = require("path");
-const express = require("express");
-const cors = require("cors");
-
-const localServerApp = express();
-const PORT = 8088;
-
-const startLocalServer = (done) => {
-  localServerApp.use(express.json({ limit: "100mb" }));
-  localServerApp.use(cors());
-  localServerApp.use(express.static("./src/dist/"));
-  localServerApp.listen(PORT, async () => {
-    console.log("Server Started on PORT ", PORT);
-    done();
-  });
-};
 
 function createWindow() {
   // Create the browser window.
@@ -52,7 +37,7 @@ function createWindow() {
   });
 
   // Create a tray icon
-  const tray = new Tray(path.join(__dirname, "dist/favicon.ico"));
+  const tray = new Tray(path.join(__dirname, "favicon.ico"));
 
   const contextMenu = Menu.buildFromTemplate([
     {
@@ -75,16 +60,14 @@ function createWindow() {
   });
 
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-    // open url in a browser and prevent default
     shell.openExternal(url);
     return { action: "deny" };
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL("http://localhost:" + PORT);
+  mainWindow.loadURL("https://app.dysperse.com");
 
   const setColor = () => {
-    // if window is out of focus, don't change the color
     if (!mainWindow.isFocused()) return;
 
     const metaTags = mainWindow.webContents.executeJavaScript(`
@@ -115,30 +98,11 @@ function createWindow() {
     setColor();
     setInterval(setColor, 1000);
   });
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  startLocalServer(createWindow);
-
-  app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
