9873639eb51637d6420b8c4643636e129dba9f63
Manu G <manusvathgurudath@gmail.com>
2024-05-05 23:33:22 -0700

    W

--- app/(app)/[...404].tsx
+++ app/(app)/[...404].tsx
@@ -1,10 +1,24 @@
 import ContentWrapper from "@/components/layout/content";
+import Emoji from "@/ui/Emoji";
 import Text from "@/ui/Text";
 
 export default function Page() {
   return (
-    <ContentWrapper>
-      <Text>This page does not exist </Text>
+    <ContentWrapper
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+      }}
+    >
+      <Emoji size={60} emoji="1F494" style={{ marginBottom: 10 }} />
+      <Text style={{ fontSize: 40 }} weight={900}>
+        Oh no!
+      </Text>
+      <Text style={{ fontSize: 20, opacity: 0.5 }} weight={500}>
+        404 | This page does not exist
+      </Text>
     </ContentWrapper>
   );
 }
