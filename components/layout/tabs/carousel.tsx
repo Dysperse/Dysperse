e18363e78b060b5f15ff4500b1167e0f10e372ad
Manu G <manusvathgurudath@gmail.com>
2024-05-06 17:47:25 +0000

    W

--- components/layout/tabs/carousel.tsx
+++ components/layout/tabs/carousel.tsx
@@ -226,7 +226,7 @@ const WebPWAInstallButton = () => {
 
 const OpenTabsList = memo(function OpenTabsList() {
   const { tab }: { tab: string } = useTabParams() as any;
-  const { data, error } = useSWR(["user/tabs"], { refreshInterval: 15000 });
+  const { data, error } = useSWR(["user/tabs"], { refreshInterval: 60000 });
 
   const handleSnapToIndex = (index: number) => {
     if (error)
