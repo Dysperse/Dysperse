132342f6d660aa76b887ede5da0adf9c32cb8c19
Manu G <manusvathgurudath@gmail.com>
2024-05-07 01:38:27 -0700

    W

--- context/useUser.tsx
+++ context/useUser.tsx
@@ -3,11 +3,9 @@ import { useSession } from "./AuthProvider";
 
 export function useUser() {
   const { session } = useSession() || {};
-  const { data, isLoading, error, mutate } = useSWR([
-    "session",
-    { token: session },
-    undefined,
-  ]);
+  const { data, isLoading, error, mutate } = useSWR(
+    session ? ["session", { token: session }, undefined] : null
+  );
 
   return { sessionToken: session, session: data, isLoading, error, mutate };
 }
