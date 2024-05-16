10a6c285779646228f2c98ceec4cb57b4885000f
Manu G <manusvathgurudath@gmail.com>
2024-05-16 00:41:46 -0700

    W

--- app/(app)/settings/account/integrations/index.tsx
+++ app/(app)/settings/account/integrations/index.tsx
@@ -1,14 +1,36 @@
 import { SettingsLayout } from "@/components/settings/layout";
 import { settingStyles } from "@/components/settings/settingsStyles";
 import Alert from "@/ui/Alert";
+import { Avatar } from "@/ui/Avatar";
 import Icon from "@/ui/Icon";
 import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
 import { useColorTheme } from "@/ui/color/theme-provider";
 import { Image } from "expo-image";
 import { router } from "expo-router";
-import { Pressable, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import useSWR from "swr";
+
+const styles = StyleSheet.create({
+  loadingContainer: { flex: 1, alignItems: "center", justifyContent: "center" },
+  button: {
+    flex: 1,
+    padding: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    minHeight: 80,
+    alignItems: "center",
+    gap: 20,
+    flexDirection: "row",
+  },
+  image: {
+    borderRadius: 5,
+    width: 30,
+    height: 30,
+  },
+  name: { fontSize: 16 },
+  description: { opacity: 0.6 },
+});
 
 function AllIntegrations({ connected }) {
   const theme = useColorTheme();
@@ -20,9 +42,7 @@ function AllIntegrations({ connected }) {
   return (
     <>
       {!data ? (
-        <View
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
+        <View style={styles.loadingContainer}>
           <Spinner />
         </View>
       ) : (
@@ -30,43 +50,32 @@ function AllIntegrations({ connected }) {
           {data.map((integration) => (
             <Pressable
               key={integration.name}
-              style={({ pressed, hovered }) => ({
-                flex: 1,
-                padding: 10,
-                paddingHorizontal: 20,
-                borderRadius: 20,
-                minHeight: 80,
-                alignItems: "center",
-                gap: 20,
-                flexDirection: "row",
-                backgroundColor: theme[pressed ? 4 : hovered ? 3 : 2],
-                opacity:
-                  integration.name === "Notion" || integration.name === "Gmail"
-                    ? 0.5
-                    : 1,
-              })}
-              onPress={() => {
+              style={({ pressed, hovered }) => [
+                styles.button,
+                {
+                  backgroundColor: theme[pressed ? 4 : hovered ? 3 : 2],
+                  opacity:
+                    integration.name === "Notion" ||
+                    integration.name === "Gmail"
+                      ? 0.5
+                      : 1,
+                },
+              ]}
+              onPress={() =>
                 router.replace(
                   `/settings/account/integrations/${integration.slug}`
-                );
-              }}
+                )
+              }
               disabled={
                 integration.name === "Notion" || integration.name === "Gmail"
               }
             >
-              <Image
-                source={{ uri: integration.icon }}
-                style={{
-                  borderRadius: 5,
-                  width: 30,
-                  height: 30,
-                }}
-              />
+              <Image source={{ uri: integration.icon }} style={styles.image} />
               <View style={{ flex: 1 }}>
-                <Text numberOfLines={1} weight={700} style={{ fontSize: 16 }}>
+                <Text numberOfLines={1} weight={700} style={styles.name}>
                   {integration.name}
                 </Text>
-                <Text numberOfLines={1} weight={300} style={{ opacity: 0.6 }}>
+                <Text numberOfLines={1} weight={300} style={styles.description}>
                   {integration.description}
                 </Text>
               </View>
@@ -80,6 +89,7 @@ function AllIntegrations({ connected }) {
 }
 
 export default function Page() {
+  const theme = useColorTheme();
   const { data } = useSWR(["space/integrations"]);
 
   return (
@@ -97,6 +107,25 @@ export default function Page() {
       Notion, Google Calendar, and more."
       />
 
+      <Pressable
+        style={({ pressed, hovered }) => [
+          styles.button,
+          {
+            backgroundColor: theme[pressed ? 4 : hovered ? 3 : 2],
+          },
+        ]}
+      >
+        <Avatar icon="email" size={40} />
+        <View style={{ flex: 1 }}>
+          <Text numberOfLines={1} weight={700} style={styles.name}>
+            Email forwarding
+          </Text>
+          <Text numberOfLines={1} weight={300} style={styles.description}>
+            Send emails to tasks@dysperse.com and they'll show up in your list!
+          </Text>
+        </View>
+        <Icon>check</Icon>
+      </Pressable>
       <AllIntegrations connected={data} />
     </SettingsLayout>
   );
