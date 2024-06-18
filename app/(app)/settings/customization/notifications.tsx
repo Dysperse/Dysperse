2971b07aa563fcd45fcc76f53887d6bb0b2df873
Manu G <manusvathgurudath@gmail.com>
2024-06-17 22:52:36 -0700

    W

--- app/(app)/settings/customization/notifications.tsx
+++ app/(app)/settings/customization/notifications.tsx
@@ -1,16 +1,34 @@
 import { SettingsLayout } from "@/components/settings/layout";
 import { settingStyles } from "@/components/settings/settingsStyles";
+import { useSession } from "@/context/AuthProvider";
+import { sendApiRequest } from "@/helpers/api";
 import Alert from "@/ui/Alert";
 import { Button, ButtonText } from "@/ui/Button";
+import { addHslAlpha } from "@/ui/color";
+import { useColorTheme } from "@/ui/color/theme-provider";
+import ErrorAlert from "@/ui/Error";
 import Icon from "@/ui/Icon";
+import IconButton from "@/ui/IconButton";
 import { ListItemButton } from "@/ui/ListItemButton";
 import ListItemText from "@/ui/ListItemText";
+import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
 import Constants from "expo-constants";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Platform, View } from "react-native";
+import Toast from "react-native-toast-message";
+import useSWR from "swr";
+
+const type = ["Unknown", "Phone", "Tablet", "Desktop", "TV"];
+const icons = [
+  "device_unknown",
+  "phone_iphone",
+  "tablet_mac",
+  "desktop_mac",
+  "tv_gen",
+];
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -20,35 +38,12 @@ Notifications.setNotificationHandler({
   }),
 });
 
-async function sendPushNotification(expoPushToken: string) {
-  const message = {
-    to: expoPushToken,
-    sound: "default",
-    title: "Original Title",
-    body: "And here is the body!",
-    data: { someData: "goes here" },
-  };
-
-  await fetch("https://exp.host/--/api/v2/push/send", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Accept-encoding": "gzip, deflate",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(message),
-  });
-}
-
-function handleRegistrationError(errorMessage: string) {
-  alert(errorMessage);
-  throw new Error(errorMessage);
-}
-
-async function registerForPushNotificationsAsync() {
+async function registerForPushNotificationsAsync(session) {
   if (Platform.OS === "android") {
     Notifications.setNotificationChannelAsync("default", {
-      name: "default",
+      name: "All notifications",
+      description:
+        "Open Dysperse to change your notification settings at any time 🚀",
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
       lightColor: "#FF231F7C",
@@ -64,16 +59,16 @@ async function registerForPushNotificationsAsync() {
       finalStatus = status;
     }
     if (finalStatus !== "granted") {
-      handleRegistrationError(
-        "Permission not granted to get push token for push notification!"
-      );
+      console.error("Permission not granted to get push token!");
+      Toast.show({ type: "error" });
       return;
     }
     const projectId =
       Constants?.expoConfig?.extra?.eas?.projectId ??
       Constants?.easConfig?.projectId;
     if (!projectId) {
-      handleRegistrationError("Project ID not found");
+      console.log("Project ID not found");
+      Toast.show({ type: "error" });
     }
     try {
       const pushTokenString = (
@@ -81,13 +76,77 @@ async function registerForPushNotificationsAsync() {
           projectId,
         })
       ).data;
-      console.log(pushTokenString);
+
+      const data = await sendApiRequest(
+        session,
+        "POST",
+        "user/notifications",
+        {},
+        {
+          body: JSON.stringify({
+            type: "EXPO",
+            tokens: pushTokenString,
+            deviceType: Device.deviceType,
+            deviceName:
+              Device.deviceName ||
+              (Platform.OS === "web"
+                ? navigator.userAgent.split("(")[1].split(";")[0]
+                : "Unknown device"),
+          }),
+        }
+      );
+      console.error(data);
       return pushTokenString;
     } catch (e: unknown) {
-      handleRegistrationError(`${e}`);
+      console.error(e);
+      Toast.show({ type: "error" });
     }
   } else {
-    handleRegistrationError("Must use physical device for push notifications");
+    console.error("Must use physical device for push notifications");
+    Toast.show({ type: "error" });
+  }
+}
+
+async function registerForWebPushNotificationsAsync(session) {
+  const base64ToUint8Array = (base64) => {
+    const padding = "=".repeat((4 - (base64.length % 4)) % 4);
+    const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+    const rawData = window.atob(b64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  };
+  if (
+    typeof window !== "undefined" &&
+    "serviceWorker" in navigator &&
+    window.workbox !== undefined
+  ) {
+    // run only in browser
+    navigator.serviceWorker.ready.then(async (reg) => {
+      reg.pushManager.getSubscription().then((sub) => {
+        if (
+          sub &&
+          !(
+            sub.expirationTime &&
+            Date.now() > sub.expirationTime - 5 * 60 * 1000
+          )
+        ) {
+          console.log(sub);
+          return sub;
+        }
+      });
+      await reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: base64ToUint8Array(
+          process.env.EXPO_PUBLIC_WEB_PUSH_API_KEY
+        ),
+      });
+      console.log("web push subscribed!");
+    });
   }
 }
 
@@ -170,69 +229,61 @@ const notificationSettings = [
   },
 ];
 
-export default function Page() {
-  const values = {
-    "task-reminders": true,
+const TestNotifications = () => {
+  const { session } = useSession();
+  const handleTest = async () => {
+    await sendApiRequest(session, "POST", "user/notifications/test");
   };
-  const [settings, setSettings] = useState(values);
 
-  const [expoPushToken, setExpoPushToken] = useState("");
-  const [notification, setNotification] = useState<
-    Notifications.Notification | undefined
-  >(undefined);
-  const notificationListener = useRef<Notifications.Subscription>();
-  const responseListener = useRef<Notifications.Subscription>();
+  return (
+    <Button
+      onPress={handleTest}
+      variant="filled"
+      large
+      style={{ marginTop: 20 }}
+    >
+      <ButtonText style={{ fontSize: 20 }} weight={700}>
+        Show an example
+      </ButtonText>
+      <Icon bold>send</Icon>
+    </Button>
+  );
+};
+
+export default function Page() {
+  const { session } = useSession();
+  const theme = useColorTheme();
+
+  const { data, mutate, error } = useSWR(["user/notifications"]);
+
+  const [settings, setSettings] = useState({ "task-reminders": true });
 
   useEffect(() => {
-    registerForPushNotificationsAsync()
-      .then((token) => setExpoPushToken(token ?? ""))
-      .catch((error: any) => setExpoPushToken(`${error}`));
+    if (Platform.OS === "web") registerForWebPushNotificationsAsync(session);
+    else registerForPushNotificationsAsync(session);
+  }, [session]);
 
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) => {
-        setNotification(notification);
-      });
-
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
-
-    return () => {
-      notificationListener.current &&
-        Notifications.removeNotificationSubscription(
-          notificationListener.current
-        );
-      responseListener.current &&
-        Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
+  const handleDelete = async (id: string) => {
+    sendApiRequest(session, "DELETE", "user/notifications", {
+      id,
+    });
+    mutate((oldData) => oldData.filter((device) => device.id !== id), {
+      revalidate: false,
+    });
+  };
 
   return (
     <SettingsLayout>
       <Text style={settingStyles.title}>Notifications</Text>
-      <Button
-        onPress={async () => {
-          await sendPushNotification(expoPushToken);
-        }}
-      >
-        <ButtonText>Press to Send Notification</ButtonText>
-      </Button>
-      <Text>Your Expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: "center", justifyContent: "center" }}>
-        <Text>
-          Title: {notification && notification.request.content.title}{" "}
-        </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>
-          Data:{" "}
-          {notification && JSON.stringify(notification.request.content.data)}
-        </Text>
-      </View>
-
-      <Text>
+      <Text weight={600}>
         Here, you can control how much you want to be{" "}
-        <Text style={{ textDecorationLine: "line-through", opacity: 0.4 }}>
+        <Text
+          weight={600}
+          style={{
+            textDecorationLine: "line-through",
+            color: addHslAlpha(theme[12], 0.6),
+          }}
+        >
           annoyed
         </Text>{" "}
         notified.
@@ -244,6 +295,31 @@ export default function Page() {
         title="Work in progress"
         subtitle="We're working hard to add notifications to the app. Stay tuned!"
       />
+
+      <TestNotifications />
+      {data && data.length > 0 && (
+        <Text style={settingStyles.heading}>Manage devices</Text>
+      )}
+      {data ? (
+        data.map((device) => (
+          <ListItemButton variant="filled" disabled key={device.id}>
+            <Icon>{icons[device.deviceType]}</Icon>
+            <ListItemText
+              primary={device.deviceName}
+              secondary={type[device.deviceType]}
+            />
+            <IconButton
+              onPress={() => handleDelete(device.id)}
+              icon="close"
+              variant="outlined"
+            />
+          </ListItemButton>
+        ))
+      ) : error ? (
+        <ErrorAlert />
+      ) : (
+        <Spinner />
+      )}
 
       {notificationSettings.map((section) => (
         <View key={section.name} style={{ paddingHorizontal: 0 }}>
