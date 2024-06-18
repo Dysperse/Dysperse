bef2fe9b757b8e5f617619fdd49ca4b003d13bd6
Manu G <manusvathgurudath@gmail.com>
2024-06-18 00:13:12 -0700

    ?

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  globDirectory: "dist/",
+  globPatterns: ["**/*.{js,ttf,ico,html,json,css,svg,png}"],
+  swDest: "dist/sw.js",
+  // 2 gigabytes
+  maximumFileSizeToCacheInBytes: 2 * 1024 * 1024 * 1024,
+  swSrc: "public/service-worker.js",
+};
