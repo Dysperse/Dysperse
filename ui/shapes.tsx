bb6457b4632ed98c7129d09ee52fc5f78a4b997f
Manu G <manusvathgurudath@gmail.com>
2024-06-22 01:54:06 -0700

    fix stupid shape

--- ui/shapes.tsx
+++ ui/shapes.tsx
@@ -70,23 +70,16 @@ export const shape3 = (props: shapeProps) => (
 );
 
 export const shape4 = (props: shapeProps) => (
-  <svg
-    width={props.size}
-    height={props.size}
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 200 200"
-    version="1.1"
-  >
-    <g clipPath="url(#clip0_105_480)">
-      <path
+  <Svg width={props.size} height={props.size} fill="none" viewBox="0 0 200 200">
+    <G clipPath="url(#clip0_105_480)">
+      <Path
         fill={props.color}
         fillRule="evenodd"
         d="M124.899 4.917 105.8 85.998l43.827-70.838a4 4 0 0 1 6.23-.724l29.707 29.707a4 4 0 0 1-.724 6.23L114.002 94.2l81.081-19.099A4 4 0 0 1 200 78.995v42.011a4 4 0 0 1-4.917 3.893L114.002 105.8l70.838 43.828a4 4 0 0 1 .724 6.23l-29.707 29.706a4 4 0 0 1-6.23-.724L105.8 114.002l19.099 81.081a4 4 0 0 1-3.893 4.917H78.994a4 4 0 0 1-3.893-4.917l19.1-81.081-43.828 70.838a4 4 0 0 1-6.23.724l-29.707-29.707a4 4 0 0 1 .724-6.23L85.998 105.8l-81.08 19.099A4 4 0 0 1 0 121.006V78.994a4 4 0 0 1 4.917-3.893l81.081 19.1L15.16 50.372a4 4 0 0 1-.724-6.23l29.707-29.707a4 4 0 0 1 6.23.724L94.2 85.998 75.1 4.918A4 4 0 0 1 78.995 0h42.012a4 4 0 0 1 3.893 4.917ZM100 111.43c6.312 0 11.429-5.117 11.429-11.429s-5.117-11.43-11.429-11.43S88.571 93.688 88.571 100s5.117 11.429 11.429 11.429Z"
         clipRule="evenodd"
-      ></path>
-    </g>
-  </svg>
+      ></Path>
+    </G>
+  </Svg>
 );
 
 export const shape7 = (props: shapeProps) => (
