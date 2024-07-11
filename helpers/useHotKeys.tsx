959dde99efbbc91e079841253900b8a999933771
Manu G <manusvathgurudath@gmail.com>
2024-07-11 14:57:22 -0700

    W

--- helpers/useHotKeys.tsx
+++ helpers/useHotKeys.tsx
@@ -1,6 +1,6 @@
 import { HotkeyCallback, Keys } from "react-hotkeys-hook";
 import { OptionsOrDependencyArray } from "react-hotkeys-hook/dist/types";
-import { Platform } from "react-native";
+import { Dimensions, Platform } from "react-native";
 
 /**
  * Works on web only
@@ -11,5 +11,11 @@ export const useHotkeys: (
   options?: OptionsOrDependencyArray,
   dependencies?: OptionsOrDependencyArray
 ) => any =
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  Platform.OS === "web" ? require("react-hotkeys-hook").useHotkeys : () => {};
+  Platform.OS === "web" &&
+  !(
+    Dimensions.get("window").width === 400 &&
+    Dimensions.get("window").height === 400
+  )
+    ? // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("react-hotkeys-hook").useHotkeys
+    : () => {};
