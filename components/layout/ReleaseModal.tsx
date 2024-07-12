07a50733a07b88c8ff1431ee770ce618303248c4
Manu G <manusvathgurudath@gmail.com>
2024-07-12 15:34:09 -0700

    asdf

--- components/layout/ReleaseModal.tsx
+++ components/layout/ReleaseModal.tsx
@@ -59,7 +59,7 @@ const ReleaseModal = memo(() => {
   const breakpoints = useResponsiveBreakpoints();
   return (
     <>
-      {(updateExists || true) && (
+      {updateExists && (
         <View
           style={{
             position: "absolute",
