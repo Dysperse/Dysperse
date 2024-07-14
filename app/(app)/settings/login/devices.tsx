0c8a81938f2924046acf7f09a6039d678513a2b8
Manu G <manusvathgurudath@gmail.com>
2024-07-13 17:51:00 -0700

    W

--- app/(app)/settings/login/devices.tsx
+++ app/(app)/settings/login/devices.tsx
@@ -187,7 +187,7 @@ export default function Page() {
   };
 
   return (
-    <SettingsScrollView noScroll>
+    <SettingsScrollView>
       {Array.isArray(data) ? (
         <FlashList
           estimatedItemSize={330}
