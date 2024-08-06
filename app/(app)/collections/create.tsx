3222df5faf34310b15eab3ca29a5a432ac882134
Manu G <manusvathgurudath@gmail.com>
2024-08-05 22:57:36 -0700

    m

--- app/(app)/collections/create.tsx
+++ app/(app)/collections/create.tsx
@@ -1,4 +1,5 @@
 import LabelPicker from "@/components/labels/picker";
+import ContentWrapper from "@/components/layout/content";
 import { createTab } from "@/components/layout/openTab";
 import { useUser } from "@/context/useUser";
 import { sendApiRequest } from "@/helpers/api";
@@ -8,18 +9,21 @@ import Emoji from "@/ui/Emoji";
 import { EmojiPicker } from "@/ui/EmojiPicker";
 import Icon from "@/ui/Icon";
 import IconButton from "@/ui/IconButton";
+import { Modal } from "@/ui/Modal";
 import Text from "@/ui/Text";
 import TextField from "@/ui/TextArea";
-import { addHslAlpha, useDarkMode } from "@/ui/color";
-import { useColorTheme } from "@/ui/color/theme-provider";
+import { useColor, useDarkMode } from "@/ui/color";
+import { ColorThemeProvider, useColorTheme } from "@/ui/color/theme-provider";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
-import { memo, useEffect, useRef, useState } from "react";
+import { cloneElement, memo, useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import {
   InteractionManager,
   Linking,
+  Pressable,
   ScrollView,
   StyleSheet,
   View,
@@ -75,65 +79,125 @@ function Templates() {
   return (
     <View>
       <View
-        style={[styles.containerContent, { padding: breakpoints.md ? 50 : 20 }]}
+        style={{
+          padding: 30,
+          paddingTop: 10,
+          paddingHorizontal: breakpoints.md ? 50 : 20,
+          width: "100%",
+          alignItems: "center",
+        }}
       >
-        <Text weight={900} style={{ fontSize: 25 }}>
-          For you
+        <Text variant="eyebrow">New collection</Text>
+        <Text
+          style={{
+            fontSize: 40,
+            textAlign: "center",
+            color: theme[11],
+          }}
+        >
+          Choose a template
         </Text>
       </View>
       <FlatList
-        horizontal
-        data={data}
+        data={["new", "dysverse", ...data]}
         contentContainerStyle={{
           padding: 20,
           paddingHorizontal: breakpoints.md ? 50 : 20,
-          gap: 10,
           paddingTop: 0,
         }}
-        style={{
-          marginTop: -10,
-        }}
+        numColumns={3}
+        showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
-          <Button
-            height={breakpoints.md ? 200 : 150}
-            onPress={() => {
-              Linking.openURL(`https://dysperse.com/templates/${item.id}`);
-            }}
+          <View
             style={{
-              width: breakpoints.md ? 300 : 200,
-              flexDirection: "column",
-              padding: 0,
-              justifyContent: "flex-start",
-            }}
-            containerStyle={{ borderRadius: 5 }}
-            variant="filled"
-            backgroundColors={{
-              default: addHslAlpha(theme[9], 0.1),
-              hovered: addHslAlpha(theme[9], 0.15),
-              pressed: addHslAlpha(theme[9], 0.2),
+              padding: 5,
+              flex: 1,
             }}
           >
-            <Image
-              source={{
-                uri: `${item.preview}?hideHeader=true&isLight=${
-                  isDark ? "false" : "true"
-                }`,
+            <Button
+              onPress={() => {
+                if (item === "new") return;
+                if (item === "dysverse")
+                  return Linking.openURL("https://dysperse.com/templates");
+                Linking.openURL(`https://dysperse.com/templates/${item.id}`);
               }}
               style={{
+                backgroundColor: "hsl(0, 0%, 90%)",
                 width: "100%",
-                aspectRatio: "1200/630",
-                borderRadius: 5,
+                flexDirection: "column",
+                padding: 0,
+                borderRadius: 15,
+                height: "auto",
+                minHeight: "auto",
+                maxHeight: "auto",
+                justifyContent: "flex-start",
               }}
-            />
-            <ButtonText weight={900}>{item.name}</ButtonText>
-          </Button>
+              containerStyle={{
+                flex: 1,
+                height: "auto",
+                minHeight: "auto",
+                maxHeight: "auto",
+                borderRadius: 15,
+                aspectRatio: "1200/630",
+              }}
+              variant="filled"
+            >
+              {item === "dysverse" ? (
+                <View
+                  style={{
+                    width: "100%",
+                    aspectRatio: "1200/630",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "row",
+                    gap: 10,
+                  }}
+                >
+                  <Icon filled>category</Icon>
+                  <ButtonText style={{ fontSize: 20 }} weight={900}>
+                    Browse the Dysverse
+                  </ButtonText>
+                </View>
+              ) : item === "new" ? (
+                <Scratch>
+                  <Pressable
+                    style={{
+                      width: "100%",
+                      aspectRatio: "1200/630",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      flexDirection: "row",
+                      gap: 10,
+                    }}
+                  >
+                    <Icon filled>add_circle</Icon>
+                    <ButtonText style={{ fontSize: 20 }} weight={900}>
+                      Start from scratch
+                    </ButtonText>
+                  </Pressable>
+                </Scratch>
+              ) : (
+                <Image
+                  source={{
+                    uri: `${item.preview}?isLight=${isDark ? "false" : "true"}`,
+                  }}
+                  style={{
+                    width: "100%",
+                    aspectRatio: "1200/630",
+                  }}
+                />
+              )}
+            </Button>
+          </View>
         )}
       />
     </View>
   );
 }
 
-export default function Page() {
+const Scratch = ({ children }) => {
+  const theme = useColorTheme();
+  const ref = useRef<BottomSheetModal>(null);
   const { session, sessionToken } = useUser();
   const [loading, setLoading] = useState(false);
   const nameRef = useRef(null);
@@ -149,12 +213,6 @@ export default function Page() {
       labels: [],
     },
   });
-
-  useEffect(() => {
-    InteractionManager.runAfterInteractions(() => {
-      nameRef.current?.focus({ preventScroll: true });
-    });
-  }, []);
 
   const onSubmit = async (data) => {
     try {
@@ -180,131 +238,157 @@ export default function Page() {
     }
   };
 
-  const theme = useColorTheme();
-  const breakpoints = useResponsiveBreakpoints();
+  const trigger = cloneElement(children, {
+    onPress: () => {
+      ref.current?.present();
+      InteractionManager.runAfterInteractions(() => {
+        nameRef.current?.focus({ preventScroll: true });
+      });
+    },
+  });
 
   return (
-    <View style={{ backgroundColor: theme[1], flex: 1 }}>
-      <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
-        <LinearGradient colors={[theme[3], theme[4]]} style={{ width: "100%" }}>
-          <Header />
-          <Templates />
-        </LinearGradient>
+    <>
+      {trigger}
+      <Modal ref={ref} animation="SCALE">
         <View
-          style={{ width: "100%", padding: breakpoints.md ? 50 : 20, gap: 20 }}
+          style={[
+            styles.containerContent,
+            { alignSelf: "center", padding: 20 },
+          ]}
         >
-          <Text weight={900} style={{ fontSize: 25 }}>
-            Start from scratch
+          <Text
+            weight={900}
+            style={{
+              fontSize: 25,
+              color: theme[11],
+            }}
+          >
+            New collection
           </Text>
           <View
-            style={[
-              styles.containerContent,
-              { alignSelf: "center", padding: breakpoints.md ? 50 : 20 },
-            ]}
+            style={{
+              flexDirection: "row",
+              gap: 20,
+            }}
           >
+            <View style={styles.section}>
+              <Text variant="eyebrow">Icon</Text>
+              <Controller
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, value } }) => (
+                  <EmojiPicker setEmoji={onChange}>
+                    <IconButton variant="filled" size={80}>
+                      <Emoji emoji={value} size={50} />
+                    </IconButton>
+                  </EmojiPicker>
+                )}
+                name="emoji"
+              />
+            </View>
             <View
               style={{
-                flexDirection: !breakpoints.md ? "column" : "row",
+                flexDirection: "column",
                 gap: 20,
+                flex: 1,
               }}
             >
-              <View style={styles.section}>
-                <Text variant="eyebrow">Icon</Text>
+              <View style={[styles.section, { flex: 1 }]}>
+                <Text variant="eyebrow">Labels</Text>
+                <Controller
+                  control={control}
+                  name="labels"
+                  render={({ field: { onChange, value } }) => (
+                    <LabelPicker
+                      multiple
+                      hideBack
+                      autoFocus={false}
+                      label={value}
+                      setLabel={onChange}
+                      onClose={() => {}}
+                    >
+                      <Button
+                        height={80}
+                        style={{
+                          borderWidth: 2,
+                          borderColor: theme[7],
+                          borderStyle: "dashed",
+                        }}
+                        variant="outlined"
+                      >
+                        <ButtonText weight={900} style={{ fontSize: 20 }}>
+                          {value.length === 0
+                            ? "Tap to select"
+                            : value.length + " selected"}
+                        </ButtonText>
+                      </Button>
+                    </LabelPicker>
+                  )}
+                />
+              </View>
+              <View style={[styles.section]}>
+                <Text variant="eyebrow">Name</Text>
                 <Controller
                   control={control}
                   rules={{ required: true }}
                   render={({ field: { onChange, value } }) => (
-                    <EmojiPicker setEmoji={onChange}>
-                      <IconButton variant="filled" size={80}>
-                        <Emoji emoji={value} size={50} />
-                      </IconButton>
-                    </EmojiPicker>
+                    <TextField
+                      variant="filled+outlined"
+                      inputRef={nameRef}
+                      placeholder={`${
+                        session?.user?.profile?.name?.split(" ")?.[0]
+                      }'s collection`}
+                      value={value}
+                      onChangeText={onChange}
+                      weight={900}
+                      style={{
+                        height: 60,
+                        borderRadius: 99,
+                        fontSize: 20,
+                        ...(errors.name && { borderColor: "red" }),
+                      }}
+                    />
                   )}
-                  name="emoji"
+                  name="name"
                 />
               </View>
-              <View
-                style={{
-                  flexDirection: "column",
-                  gap: 20,
-                  flex: 1,
-                }}
+              <Button
+                height={80}
+                variant="filled"
+                onPress={handleSubmit(onSubmit)}
+                isLoading={loading}
               >
-                <View style={[styles.section, { flex: 1 }]}>
-                  <Text variant="eyebrow">Labels</Text>
-                  <Controller
-                    control={control}
-                    name="labels"
-                    render={({ field: { onChange, value } }) => (
-                      <LabelPicker
-                        multiple
-                        hideBack
-                        autoFocus={false}
-                        label={value}
-                        setLabel={onChange}
-                        onClose={() => {}}
-                      >
-                        <Button
-                          height={80}
-                          style={{
-                            borderWidth: 2,
-                            borderColor: theme[7],
-                            borderStyle: "dashed",
-                          }}
-                          variant="outlined"
-                        >
-                          <ButtonText weight={900} style={{ fontSize: 20 }}>
-                            {value.length === 0
-                              ? "Tap to select"
-                              : value.length + " selected"}
-                          </ButtonText>
-                        </Button>
-                      </LabelPicker>
-                    )}
-                  />
-                </View>
-                <View style={[styles.section]}>
-                  <Text variant="eyebrow">Name</Text>
-                  <Controller
-                    control={control}
-                    rules={{ required: true }}
-                    render={({ field: { onChange, value } }) => (
-                      <TextField
-                        variant="filled+outlined"
-                        inputRef={nameRef}
-                        placeholder={`${
-                          session?.user?.profile?.name?.split(" ")?.[0]
-                        }'s collection`}
-                        value={value}
-                        onChangeText={onChange}
-                        weight={900}
-                        style={{
-                          height: 60,
-                          borderRadius: 99,
-                          fontSize: 20,
-                          ...(errors.name && { borderColor: "red" }),
-                        }}
-                      />
-                    )}
-                    name="name"
-                  />
-                </View>
-                <Button
-                  height={80}
-                  variant="filled"
-                  onPress={handleSubmit(onSubmit)}
-                  isLoading={loading}
-                >
-                  <ButtonText weight={900} style={{ fontSize: 20 }}>
-                    Done
-                  </ButtonText>
-                  <Icon>check</Icon>
-                </Button>
-              </View>
+                <ButtonText weight={900} style={{ fontSize: 20 }}>
+                  Done
+                </ButtonText>
+                <Icon>check</Icon>
+              </Button>
             </View>
           </View>
         </View>
-      </ScrollView>
-    </View>
+      </Modal>
+    </>
+  );
+};
+
+export default function Page() {
+  const theme = useColor("gray");
+  const breakpoints = useResponsiveBreakpoints();
+
+  return (
+    <ColorThemeProvider theme={theme}>
+      <ContentWrapper>
+        <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+          <LinearGradient
+            colors={["transparent", "hsl(0, 0%, 95%)"]}
+            style={{ width: "100%" }}
+          >
+            <Header />
+            <Templates />
+          </LinearGradient>
+        </ScrollView>
+      </ContentWrapper>
+    </ColorThemeProvider>
   );
 }
