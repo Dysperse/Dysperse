05b07aa27951995e547a7d61e8903bdcb90ce6fb
Manu G <manusvathgurudath@gmail.com>
2024-08-16 19:03:13 -0700

    W

--- components/collections/navbar/AgendaCalendarMenu.tsx
+++ components/collections/navbar/AgendaCalendarMenu.tsx
@@ -1,8 +1,6 @@
 import { useResponsiveBreakpoints } from "@/helpers/useResponsiveBreakpoints";
 import { Button } from "@/ui/Button";
-import Calendar from "@/ui/Calendar";
 import { useColorTheme } from "@/ui/color/theme-provider";
-import MenuPopover from "@/ui/MenuPopover";
 import Text from "@/ui/Text";
 import dayjs from "dayjs";
 import {
@@ -11,11 +9,28 @@ import {
   useLocalSearchParams,
 } from "expo-router";
 
+import { useDarkMode } from "@/ui/color";
+import { Modal } from "@/ui/Modal";
+import {
+  Calendar,
+  CalendarListRef,
+  fromDateId,
+  toDateId,
+} from "@marceloterreiro/flash-calendar";
+import { useRef } from "react";
+import { View } from "react-native";
+
 export function AgendaCalendarMenu() {
+  const ref = useRef(null);
+  const calendarRef = useRef<CalendarListRef>(null);
+
+  const isDark = useDarkMode();
   const theme = useColorTheme();
   const breakpoints = useResponsiveBreakpoints();
   const { start }: any = useGlobalSearchParams();
   const { agendaView, mode } = useLocalSearchParams();
+
+  const today = toDateId(dayjs(start).toDate());
 
   const titleFormat = {
     week: "[Week #]W • MMM YYYY",
@@ -26,43 +41,70 @@ export function AgendaCalendarMenu() {
   }[((agendaView || mode) as any) || "week"];
 
   return (
-    <MenuPopover
-      containerStyle={{ width: 300, marginTop: 10 }}
-      trigger={
-        <Button
-          backgroundColors={{
-            default: breakpoints.md ? "transparent" : theme[3],
-            hovered: breakpoints.md ? "transparent" : theme[4],
-            pressed: breakpoints.md ? "transparent" : theme[5],
-          }}
-          borderColors={{
-            default: breakpoints.md ? "transparent" : theme[3],
-            hovered: breakpoints.md ? "transparent" : theme[4],
-            pressed: breakpoints.md ? "transparent" : theme[5],
+    <>
+      <Button
+        onPress={() => ref.current?.present()}
+        backgroundColors={{
+          default: breakpoints.md ? "transparent" : theme[3],
+          hovered: breakpoints.md ? "transparent" : theme[4],
+          pressed: breakpoints.md ? "transparent" : theme[5],
+        }}
+        borderColors={{
+          default: breakpoints.md ? "transparent" : theme[3],
+          hovered: breakpoints.md ? "transparent" : theme[4],
+          pressed: breakpoints.md ? "transparent" : theme[5],
+        }}
+      >
+        <Text numberOfLines={1} weight={600}>
+          {dayjs(start).format(titleFormat).split("•")?.[0]}
+        </Text>
+        <Text numberOfLines={1} style={{ opacity: 0.6 }}>
+          {dayjs(start).format(titleFormat).split("• ")?.[1]}
+        </Text>
+      </Button>
+
+      <Modal animation="SCALE" ref={ref} maxWidth={300} height={500}>
+        <View
+          style={{
+            padding: 20,
+            flex: 1,
           }}
         >
-          <Text numberOfLines={1} weight={600}>
-            {dayjs(start).format(titleFormat).split("•")?.[0]}
-          </Text>
-          <Text numberOfLines={1} style={{ opacity: 0.6 }}>
-            {dayjs(start).format(titleFormat).split("• ")?.[1]}
-          </Text>
-        </Button>
-      }
-    >
-      <Calendar
-        onDayPress={(day) => {
-          router.setParams({
-            start: dayjs(day.dateString, "YYYY-MM-DD").utc().toISOString(),
-          });
-        }}
-        markedDates={{
-          [dayjs(start).format("YYYY-MM-DD")]: {
-            selected: true,
-            disableTouchEvent: true,
-          },
-        }}
-      />
-    </MenuPopover>
+          <Calendar.List
+            calendarActiveDateRanges={[
+              {
+                startId: toDateId(dayjs(start).startOf("week").toDate()),
+                endId: toDateId(dayjs(start).endOf("week").toDate()),
+              },
+            ]}
+            calendarInitialMonthId={today}
+            endFillColor={theme[3]}
+            ref={calendarRef}
+            calendarColorScheme={isDark ? "dark" : "light"}
+            onCalendarDayPress={(day) => {
+              ref.current?.forceClose({ duration: 0.0001 });
+              setTimeout(() => {
+                router.setParams({
+                  start: dayjs(fromDateId(day).toISOString(), "YYYY-MM-DD")
+                    .utc()
+                    .toISOString(),
+                });
+              }, 100);
+            }}
+          />
+          <Button
+            variant="filled"
+            height={60}
+            bold
+            onPress={() => {
+              const pastMonth = dayjs(start).startOf("month").toDate();
+              calendarRef.current?.scrollToMonth(pastMonth, true);
+            }}
+            icon="undo"
+            text="Today"
+          />
+        </View>
+      </Modal>
+    </>
   );
 }
