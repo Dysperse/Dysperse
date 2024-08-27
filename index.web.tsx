4090e7fa41b9fff97784a0ae592163588a035f52
Manu G <manusvathgurudath@gmail.com>
2024-08-26 17:40:21 -0700

    W

--- index.web.tsx
+++ index.web.tsx
@@ -1,11 +1,12 @@
 import "@expo/metro-runtime";
+import { LoadSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
+import { version } from "canvaskit-wasm/package.json";
 import { App } from "expo-router/build/qualified-entry";
 import { renderRootComponent } from "expo-router/build/renderRootComponent";
 
-import { LoadSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
-
 LoadSkiaWeb({
-  locateFile: (file) => `/${file}`,
+  locateFile: (file) =>
+    `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${version}/bin/full/${file}`,
 }).then(async () => {
   renderRootComponent(App);
 });
