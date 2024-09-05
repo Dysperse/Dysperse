4ea134f7dd3479bcb4c5f56d3b3c84f83f74d3b0
Manu G <manusvathgurudath@gmail.com>
2024-09-04 22:27:31 -0700

    ,

--- ui/Spinner/index.tsx
+++ ui/Spinner/index.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { StyleProp, ViewStyle } from "react-native";
 import Animated, {
-  interpolate,
   runOnUI,
   useAnimatedStyle,
   useSharedValue,
   withRepeat,
+  withSequence,
   withTiming,
 } from "react-native-reanimated";
-import Svg, { Path } from "react-native-svg";
+import { G, Path, Svg } from "react-native-svg";
 import { useColorTheme } from "../color/theme-provider";
 
 interface SpinnerProps {
@@ -18,7 +18,8 @@ interface SpinnerProps {
   children?: React.ReactNode;
 }
 
-const duration = 1700;
+const easing = () => 0;
+const duration = 83.333;
 
 const Spinner = (props: SpinnerProps) => {
   const theme = useColorTheme();
@@ -29,10 +30,21 @@ const Spinner = (props: SpinnerProps) => {
     "worklet"; // This annotation is important for the worklet to run on the UI thread
 
     sv.value = withRepeat(
-      withTiming(1, {
-        duration,
-        easing: (t: number) => t,
-      }),
+      // Discrete animation with 12 frames of 0.08333s each. 360/12 = 30 degrees per frame
+      withSequence(
+        withTiming(0, { duration: 0 }),
+        withTiming(30, { duration, easing }),
+        withTiming(60, { duration, easing }),
+        withTiming(90, { duration, easing }),
+        withTiming(120, { duration, easing }),
+        withTiming(150, { duration, easing }),
+        withTiming(180, { duration, easing }),
+        withTiming(210, { duration, easing }),
+        withTiming(240, { duration, easing }),
+        withTiming(270, { duration, easing }),
+        withTiming(300, { duration, easing }),
+        withTiming(330, { duration, easing })
+      ),
       -1
     );
   };
@@ -43,7 +55,7 @@ const Spinner = (props: SpinnerProps) => {
   }, [sv]);
 
   const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ rotate: `${interpolate(sv.value, [0, 1], [0, 360])}deg` }],
+    transform: [{ rotate: `${sv.value}deg` }],
     width: props.size || 20,
     height: props.size || 20,
   }));
@@ -51,14 +63,37 @@ const Spinner = (props: SpinnerProps) => {
   return (
     <Animated.View style={[animatedStyle, props.style]}>
       {props.children || (
-        <Svg
-          viewBox="0 0 100 100"
-          {...props}
-          fill={props.color || theme[11]}
-          width={props.size || 20}
-          height={props.size || 20}
-        >
-          <Path d="M50 29.41c-1.76 0-2.94-1.18-2.94-2.94V2.94C47.06 1.18 48.24 0 50 0s2.94 1.18 2.94 2.94v23.53c0 1.76-1.18 2.94-2.94 2.94M50 100c-1.76 0-2.94-1.18-2.94-2.94V73.53c0-1.76 1.18-2.94 2.94-2.94s2.94 1.18 2.94 2.94v23.53c0 1.76-1.18 2.94-2.94 2.94m11.76-67.65c-.59 0-.88 0-1.47-.29-1.18-.88-1.76-2.35-.88-3.82L71.17 7.95c.88-1.18 2.35-1.76 3.82-.88 1.18.88 1.76 2.35.88 3.82L64.11 31.18c-.59.59-1.47 1.18-2.35 1.18M26.47 93.53c-.59 0-.88 0-1.47-.29-1.18-.88-1.76-2.35-.88-3.82l11.76-20.29c.88-1.18 2.35-1.76 3.82-.88 1.18.88 1.76 2.35.88 3.82L28.82 92.36c-.59.59-1.47 1.18-2.35 1.18M38.24 32.35c-.88 0-1.76-.59-2.35-1.47L24.13 10.59c-.88-1.18-.29-2.94.88-3.82s2.94-.29 3.82.88l11.76 20.29c.88 1.18.29 2.94-.88 3.82-.59.59-.88.59-1.47.59M73.53 93.53c-.88 0-1.76-.59-2.35-1.47L59.42 71.77c-.88-1.18-.29-2.94.88-3.82s2.94-.29 3.82.88l11.76 20.29c.88 1.18.29 2.94-.88 3.82-.59.29-.88.59-1.47.59M26.47 52.94H2.94C1.18 52.94 0 51.76 0 50s1.18-2.94 2.94-2.94h23.53c1.76 0 2.94 1.18 2.94 2.94s-1.18 2.94-2.94 2.94M97.06 52.94H73.53c-1.76 0-2.94-1.18-2.94-2.94s1.18-2.94 2.94-2.94h23.53c1.76 0 2.94 1.18 2.94 2.94s-1.18 2.94-2.94 2.94M29.71 40.88c-.59 0-.88 0-1.47-.29L7.94 28.82c-1.18-.88-1.76-2.35-.88-3.82.88-1.18 2.35-1.76 3.82-.88l20.29 11.76c1.18.88 1.76 2.35.88 3.82-.59.88-1.47 1.18-2.35 1.18M90.88 76.18c-.59 0-.88 0-1.47-.29L69.12 64.13c-1.18-.88-1.76-2.35-.88-3.82.88-1.18 2.35-1.76 3.82-.88l20.29 11.76c1.18.88 1.76 2.35.88 3.82-.59.88-1.47 1.18-2.35 1.18M9.12 76.18c-.88 0-1.76-.59-2.35-1.47-.88-1.18-.29-2.94.88-3.82l20.29-11.76c1.18-.88 2.94-.29 3.82.88s.29 2.94-.88 3.82L10.59 75.59c-.29.59-.88.59-1.47.59M70.29 40.88c-.88 0-1.76-.59-2.35-1.47-.88-1.18-.29-2.94.88-3.82l20.29-11.76c1.18-.88 2.94-.29 3.82.88s.29 2.94-.88 3.82L71.76 40.29c-.59.59-.88.59-1.47.59" />
+        <Svg viewBox="0 0 2400 2400" width={props.size} height={props.size}>
+          <G
+            strokeWidth="200"
+            strokeLinecap="round"
+            stroke={props.color || theme[11]}
+            fill="none"
+          >
+            <Path d="M1200 600V100" />
+            <Path opacity=".5" d="M1200 2300v-500" />
+            <Path opacity=".917" d="M900 680.4l-250-433" />
+            <Path opacity=".417" d="M1750 2152.6l-250-433" />
+            <Path opacity=".833" d="M680.4 900l-433-250" />
+            <Path opacity=".333" d="M2152.6 1750l-433-250" />
+            <Path opacity=".75" d="M600 1200H100" />
+            <Path opacity=".25" d="M2300 1200h-500" />
+            <Path opacity=".667" d="M680.4 1500l-433 250" />
+            <Path opacity=".167" d="M2152.6 650l-433 250" />
+            <Path opacity=".583" d="M900 1719.6l-250 433" />
+            <Path opacity=".083" d="M1750 247.4l-250 433" />
+            {/* <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="rotate"
+              keyTimes="0;0.08333;0.16667;0.25;0.33333;0.41667;0.5;0.58333;0.66667;0.75;0.83333;0.91667"
+              values="0 1199 1199;30 1199 1199;60 1199 1199;90 1199 1199;120 1199 1199;150 1199 1199;180 1199 1199;210 1199 1199;240 1199 1199;270 1199 1199;300 1199 1199;330 1199 1199"
+              dur="0.83333s"
+              begin="0s"
+              repeatCount="indefinite"
+              calcMode="discrete"
+            /> */}
+          </G>
         </Svg>
       )}
     </Animated.View>
@@ -66,3 +101,4 @@ const Spinner = (props: SpinnerProps) => {
 };
 
 export default Spinner;
+
