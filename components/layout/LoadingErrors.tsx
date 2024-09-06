2c5fd2ff2414a1481714d8254d81f84a21229210
Manu G <manusvathgurudath@gmail.com>
2024-09-06 00:26:26 -0700

    W

--- components/layout/LoadingErrors.tsx
+++ components/layout/LoadingErrors.tsx
@@ -1,11 +1,13 @@
 import { useStorageContext } from "@/context/storageContext";
 import { useUser } from "@/context/useUser";
 import Icon from "@/ui/Icon";
+import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
 import { useColor } from "@/ui/color";
 import { useColorTheme } from "@/ui/color/theme-provider";
+import dayjs from "dayjs";
 import { memo } from "react";
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const LoadingErrors = memo(() => {
@@ -16,43 +18,71 @@ const LoadingErrors = memo(() => {
   const { error: storageError, isStorageValidating } = useStorageContext();
   const insets = useSafeAreaInsets();
 
+  const isTimeZoneDifference = session?.user?.timeZone === dayjs.tz.guess();
+
   return (
-    (error || storageError) && (
-      <View
-        style={[
-          {
-            backgroundColor: theme[11],
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            height: 30 + insets.top,
-            marginBottom: -insets.top,
-            paddingTop: insets.top,
-            zIndex: 999,
-            gap: 10,
-          },
-          (isValidating || isStorageValidating) && {
-            backgroundColor: orange[11],
-          },
-        ]}
-      >
-        <Icon style={{ color: red[2] }} bold size={18}>
-          cloud_off
-        </Icon>
-        <Text
-          style={{ color: red[2], fontSize: 12, marginBottom: -1 }}
-          weight={700}
+    <>
+      {(error || storageError || isTimeZoneDifference) && (
+        <View
+          style={[
+            {
+              backgroundColor: theme[11],
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              height: 40 + insets.top,
+              marginBottom: -insets.top,
+              paddingTop: insets.top,
+              zIndex: 999,
+              gap: 20,
+            },
+            (isValidating || isStorageValidating) &&
+              !isTimeZoneDifference && {
+                backgroundColor: orange[11],
+              },
+          ]}
         >
-          {isValidating || isStorageValidating
-            ? "Reconnecting…"
-            : error
-            ? error.message === "Failed to fetch"
-              ? "You're offline"
-              : "Can't connect to Dysperse"
-            : "Can't load storage data"}
-        </Text>
-      </View>
-    )
+          <StatusBar barStyle="dark-content" />
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+            {isValidating || isStorageValidating ? (
+              <Spinner size={18} color={red[2]} />
+            ) : (
+              <Icon style={{ color: red[2] }} bold size={18}>
+                cloud_off
+              </Icon>
+            )}
+            <Text
+              style={{ color: red[2], fontSize: 12, marginBottom: -1 }}
+              weight={700}
+            >
+              {error
+                ? error.message === "Failed to fetch"
+                  ? "You're offline"
+                  : "Offline"
+                : "Offline"}
+            </Text>
+          </View>
+          {isTimeZoneDifference && (
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+              }}
+            >
+              <Icon style={{ color: red[2] }} bold size={18}>
+                travel
+              </Icon>
+              <View>
+                <Text style={{ color: red[2], fontSize: 12 }} weight={900}>
+                  {dayjs.tz.guess()}
+                </Text>
+              </View>
+            </View>
+          )}
+        </View>
+      )}
+    </>
   );
 });
 
