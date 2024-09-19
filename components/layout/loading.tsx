97c6977504e04942eb8945e9a369f146e5aa6ec0
Manu G <manusvathgurudath@gmail.com>
2024-09-18 20:09:33 -0700

    W

--- components/layout/loading.tsx
+++ components/layout/loading.tsx
@@ -1,21 +1,25 @@
 import { useColorTheme } from "@/ui/color/theme-provider";
 import Logo from "@/ui/logo";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 
 export function SessionLoadingScreen() {
   const theme = useColorTheme();
 
   return (
     <View
-      style={{
-        flex: 1,
-        backgroundColor: theme[2],
-        alignItems: "center",
-        justifyContent: "center",
-        gap: 20,
-      }}
+      style={[
+        {
+          flex: 1,
+          backgroundColor: theme[2],
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 20,
+        },
+        Platform.OS === "web" && ({ WebkitAppRegion: "drag" } as any),
+      ]}
     >
       <Logo size={150} />
     </View>
   );
 }
+
