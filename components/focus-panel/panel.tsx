1958f6442639889bd2d32905e157336d34c38ca8
Manu G <manusvathgurudath@gmail.com>
2024-09-22 01:17:03 -0700

    W

--- components/focus-panel/panel.tsx
+++ components/focus-panel/panel.tsx
@@ -5,7 +5,7 @@ import { useResponsiveBreakpoints } from "@/helpers/useResponsiveBreakpoints";
 import Chip from "@/ui/Chip";
 import Icon from "@/ui/Icon";
 import IconButton from "@/ui/IconButton";
-import { MenuOption } from "@/ui/MenuPopover";
+import MenuPopover, { MenuOption } from "@/ui/MenuPopover";
 import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
 import { useColor, useDarkMode } from "@/ui/color";
@@ -572,26 +572,39 @@ function PanelContent() {
         </Stack.Navigator>
       </NavigationContainer>
       {panelState === "COLLAPSED" && (
-        <View style={{ flexDirection: "row" }}>
-          <IconButton
-            onPress={() => {
-              setPanelState("OPEN");
-              collapseOnBack.current = false;
-            }}
-            icon="expand_content"
-            size={85 / 2}
-            style={{ height: 40, opacity: 0.6, marginBottom: 5 }}
-          />
-          <IconButton
-            onPress={() => {
-              setPanelState("CLOSED");
-              collapseOnBack.current = true;
-            }}
-            icon="close_small"
-            size={85 / 2}
-            style={{ height: 40, opacity: 0.6, marginBottom: 5 }}
-          />
-        </View>
+        <MenuPopover
+          trigger={
+            <IconButton
+              style={{
+                height: 40,
+                alignSelf: "center",
+                opacity: 0.3,
+                marginBottom: 5,
+              }}
+              onPress={() => setPanelState("OPEN")}
+              icon="more_horiz"
+              iconProps={{ bold: true }}
+            />
+          }
+          options={[
+            {
+              text: "Expand",
+              icon: "expand_content",
+              callback: () => {
+                setPanelState("OPEN");
+                collapseOnBack.current = false;
+              },
+            },
+            {
+              text: "Close",
+              icon: "close",
+              callback: () => {
+                setPanelState("CLOSED");
+                collapseOnBack.current = true;
+              },
+            },
+          ]}
+        />
       )}
     </Animated.View>
   );
