06c5233668a16e10fa7d44eaaa208771902b81de
Manu G <manusvathgurudath@gmail.com>
2024-09-22 00:17:08 -0700

    W

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -5,7 +5,7 @@ import { toastConfig } from "@/ui/toast.config";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { PortalProvider } from "@gorhom/portal";
 import { TransitionPresets } from "@react-navigation/stack";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Platform, View, useWindowDimensions } from "react-native";
 import { MenuProvider } from "react-native-popup-menu";
 import Toast from "react-native-toast-message";
@@ -14,13 +14,18 @@ export default function Layout() {
   const theme = useColorTheme();
   const breakpoints = useResponsiveBreakpoints();
   const { height, width } = useWindowDimensions();
+  const [hidden, setHidden] = useState(false);
 
   useEffect(() => {
     if (Platform.OS === "web" && window.self !== window.top) {
       // Block the app from being rendered in an iframe
-      window.location.href = "https://dysperse.com";
+      setHidden(true);
     }
   }, []);
+
+  if (hidden) {
+    return <View />;
+  }
 
   return (
     <BottomSheetModalProvider>
