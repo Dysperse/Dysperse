9559305647e442fd55d356cde4825e8f4e4e5e60
Manu G <manusvathgurudath@gmail.com>
2024-11-03 11:27:15 -0800

    W

--- components/layout/arcAnimations.tsx
+++ components/layout/arcAnimations.tsx
@@ -50,7 +50,7 @@ export function arcAnimation({
   );
 
   const opacity = progress.interpolate({
-    inputRange: [0, 0.75, 0.875, 1, 1.0825, 1.2075, 2],
+    inputRange: [0, 0.5, 0.775, 1, 1.0825, 1.2075, 2],
     outputRange: [0, 0, 1, 1, 1, 1, 0.5],
   });
 
