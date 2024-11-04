8771120902fdf51ca80b0b9d7c9b95f54201c673
Manu G <manusvathgurudath@gmail.com>
2024-11-04 19:42:09 +0000

    W

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -5,8 +5,13 @@ import { useCallback, useEffect } from "react";
 export default function Page() {
   const setCurrentPage = useCallback(async () => {
     const lastViewedRoute = await AsyncStorage.getItem("lastViewedRoute");
-    if (lastViewedRoute)
-      router.replace(lastViewedRoute === "/" ? "/home" : lastViewedRoute);
+    router.replace(
+      lastViewedRoute
+        ? lastViewedRoute === "/"
+          ? "/home"
+          : lastViewedRoute
+        : "/"
+    );
   }, []);
 
   useEffect(() => {
