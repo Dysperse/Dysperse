fca2d3e85b2be18abac2509ec73ad2fcfc7ce0dd
Manu G <manusvathgurudath@gmail.com>
2024-11-08 19:27:48 +0000

    W

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -383,6 +383,7 @@ export default function AppLayout() {
                                     (pathname.includes("collections") &&
                                       (pathname.includes("/search") ||
                                         pathname.includes("/customize") ||
+                                        pathname.includes("/reorder") ||
                                         pathname.includes("/share")))
                                       ? "locked-closed"
                                       : "unlocked"
