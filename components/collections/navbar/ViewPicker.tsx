a0bcb2d6e0d36e6f10e19cbdbba45685cdda0c29
Manu G <manusvathgurudath@gmail.com>
2024-11-17 11:57:28 -0800

    W

--- components/collections/navbar/ViewPicker.tsx
+++ components/collections/navbar/ViewPicker.tsx
@@ -1,13 +1,20 @@
 import { COLLECTION_VIEWS } from "@/components/layout/command-palette/list";
 import { useResponsiveBreakpoints } from "@/helpers/useResponsiveBreakpoints";
+import { Button } from "@/ui/Button";
+import { addHslAlpha } from "@/ui/color";
 import { useColorTheme } from "@/ui/color/theme-provider";
 import Icon from "@/ui/Icon";
-import MenuPopover from "@/ui/MenuPopover";
+import { ListItemButton } from "@/ui/ListItemButton";
+import ListItemText from "@/ui/ListItemText";
+import Modal from "@/ui/Modal";
 import Text from "@/ui/Text";
 import capitalizeFirstLetter from "@/utils/capitalizeFirstLetter";
+import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { BlurView } from "expo-blur";
 import { router, useGlobalSearchParams } from "expo-router";
-import { memo } from "react";
-import { Pressable, View } from "react-native";
+import { memo, useEffect, useRef } from "react";
+import { Platform, View } from "react-native";
+import Toast from "react-native-toast-message";
 import { groupedViews } from ".";
 import { useCollectionContext } from "../context";
 import { NavbarEyebrow } from "./NavbarEyebrow";
@@ -19,6 +26,7 @@ export const ViewPicker = memo(({ isLoading }: { isLoading: any }) => {
   const { data, type } = useCollectionContext();
   const { id } = useGlobalSearchParams();
   const breakpoints = useResponsiveBreakpoints();
+  const ref = useRef(null);
   const isAll = id === "all";
 
   // Convert grouped object to array of arrays
@@ -27,63 +35,174 @@ export const ViewPicker = memo(({ isLoading }: { isLoading: any }) => {
       {
         renderer: () => (
           <Text variant="eyebrow" style={{ padding: 10, paddingBottom: 3 }}>
-            {capitalizeFirstLetter(_type)}
+            By {_type.replaceAll("Based", "")}
           </Text>
         ),
       },
       ...views.map((e) => ({
         icon: COLLECTION_VIEWS[e].icon,
         text: capitalizeFirstLetter(e),
+        secondary: COLLECTION_VIEWS[e].description,
         callback: () => router.setParams({ type: e }),
         selected: e === type,
       })),
     ])
     .flat();
 
+  const lastPress = useRef(0);
+
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      const handleKeyDown = (e) => {
+        if (
+          e.key === "Shift" &&
+          document.activeElement?.tagName !== "INPUT" &&
+          document.activeElement?.tagName !== "TEXTAREA"
+        ) {
+          const now = Date.now();
+          if (now - lastPress.current < 500) ref.current?.present();
+          lastPress.current = now;
+        }
+      };
+      window.addEventListener("keyup", handleKeyDown);
+      return () => {
+        window.removeEventListener("keyup", handleKeyDown);
+      };
+    }
+  }, []);
+
   return (
-    <MenuPopover
-      menuProps={{ rendererProps: { placement: "bottom" } }}
-      trigger={
-        <Pressable
-          android_ripple={{ color: theme[5] }}
-          style={() => ({
-            maxWidth: breakpoints.md ? 240 : "100%",
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 13,
-            paddingLeft: 10,
-            minWidth: 0,
-          })}
-        >
-          {data?.emoji && (
-            <NavbarIcon
-              isAll={isAll}
-              emoji={data.emoji}
-              isLoading={isLoading}
+    <>
+      <Button
+        onPress={() => {
+          ref.current?.present();
+          if (Platform.OS === "web" && breakpoints.md) {
+            const t = localStorage.getItem("shiftShiftTip");
+            if (t !== "true") {
+              Toast.show({
+                type: "info",
+                text1: "Pro tip",
+                text2: "Press shift twice to open this dialog!",
+              });
+              localStorage.setItem("shiftShiftTip", "true");
+            }
+          }
+        }}
+        android_ripple={{ color: theme[5] }}
+        containerStyle={{
+          padding: 0,
+          transformOrigin: breakpoints.md ? "left bottom" : undefined,
+          maxWidth: breakpoints.md ? 240 : "100%",
+        }}
+        style={() => ({
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          gap: 13,
+          paddingLeft: 10,
+          minWidth: 0,
+        })}
+      >
+        {data?.emoji && (
+          <NavbarIcon isAll={isAll} emoji={data.emoji} isLoading={isLoading} />
+        )}
+        <View style={{ minWidth: 0, flexShrink: 1 }}>
+          <NavbarEyebrow name={type} />
+          {isLoading ? (
+            <View
+              style={{
+                width: 60,
+                height: 17,
+                borderRadius: 999,
+                backgroundColor: theme[4],
+              }}
             />
+          ) : (
+            <NavbarTitle name={data.name || "All tasks"} />
           )}
-          <View style={{ minWidth: 0, flexShrink: 1 }}>
-            <NavbarEyebrow name={type} />
-            {isLoading ? (
-              <View
-                style={{
-                  width: 60,
-                  height: 17,
-                  borderRadius: 999,
-                  backgroundColor: theme[4],
-                }}
-              />
-            ) : (
-              <NavbarTitle name={data.name || "All tasks"} />
-            )}
-          </View>
-          <Icon size={30} style={{ marginLeft: -5 }}>
-            expand_more
-          </Icon>
-        </Pressable>
-      }
-      options={options}
-    />
+        </View>
+        <Icon size={30} style={{ marginLeft: -5 }}>
+          expand_more
+        </Icon>
+      </Button>
+
+      <Modal
+        transformCenter
+        animation={breakpoints.md ? "SCALE" : "SLIDE"}
+        sheetRef={ref}
+        maxWidth={400}
+        innerStyles={{
+          backgroundColor: "transparent",
+          height: "100%",
+          justifyContent: "center",
+        }}
+      >
+        <BlurView
+          style={{
+            borderRadius: 25,
+            maxHeight: "100%",
+            overflow: "hidden",
+          }}
+        >
+          <BottomSheetScrollView
+            style={{
+              height: "auto",
+              padding: 10,
+              backgroundColor: addHslAlpha(
+                theme[2],
+                Platform.OS === "android" ? 1 : 0.7
+              ),
+            }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontFamily: "serifText800",
+                fontSize: 30,
+                color: theme[11],
+                marginVertical: 20,
+              }}
+            >
+              Select a view
+            </Text>
+            <View style={{ gap: 1 }}>
+              {options.map((r, i) =>
+                r.renderer ? (
+                  r.renderer()
+                ) : (
+                  <ListItemButton
+                    key={i}
+                    onPress={() => {
+                      r.callback();
+                      ref.current?.forceClose();
+                    }}
+                    backgroundColors={{
+                      default: addHslAlpha(theme[8], r.selected ? 0.2 : 0),
+                      hover: addHslAlpha(theme[8], 0.3),
+                      active: addHslAlpha(theme[8], 0.4),
+                    }}
+                  >
+                    <Icon>{r.icon}</Icon>
+                    <ListItemText
+                      primaryProps={{
+                        style: { color: theme[11] },
+                        weight: 800,
+                      }}
+                      secondaryProps={{
+                        style: { color: theme[11], opacity: 0.5 },
+                      }}
+                      primary={r.text}
+                      secondary={r.selected && r.secondary}
+                    />
+                  </ListItemButton>
+                )
+              )}
+            </View>
+          </BottomSheetScrollView>
+        </BlurView>
+      </Modal>
+    </>
   );
 });
 
