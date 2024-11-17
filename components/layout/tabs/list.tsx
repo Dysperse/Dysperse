47e1da4351c04b83c949a568a421192add943c1c
Manu G <manusvathgurudath@gmail.com>
2024-11-16 16:02:03 -0800

    W

--- components/layout/tabs/list.tsx
+++ components/layout/tabs/list.tsx
@@ -56,6 +56,7 @@ export function SortableItem({
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
+    WebkitAppRegion: "drag",
   };
 
   const tabName = capitalizeFirstLetter(
