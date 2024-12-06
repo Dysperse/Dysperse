bb1be1d03886f2010042fa306445872bac465545
Manu G <manusvathgurudath@gmail.com>
2024-12-05 17:31:05 -0800

    Implement AI integration feature and enhance error alert styling

--- app/(app)/settings/sidekick.tsx
+++ app/(app)/settings/sidekick.tsx
@@ -1,55 +1,204 @@
+import { settingStyles } from "@/components/settings/settingsStyles";
+import { useUser } from "@/context/useUser";
+import { sendApiRequest } from "@/helpers/api";
+import { Button } from "@/ui/Button";
 import { useColorTheme } from "@/ui/color/theme-provider";
+import ErrorAlert from "@/ui/Error";
 import Icon from "@/ui/Icon";
+import { ListItemButton } from "@/ui/ListItemButton";
+import ListItemText from "@/ui/ListItemText";
+import SettingsScrollView from "@/ui/SettingsScrollView";
 import Text from "@/ui/Text";
-import { ImageBackground } from "expo-image";
+import TextField from "@/ui/TextArea";
+import { Image, ImageBackground } from "expo-image";
+import { useRef, useState } from "react";
 import { View } from "react-native";
+import Accordion from "react-native-collapsible/Accordion";
+import Toast from "react-native-toast-message";
+import useSWR from "swr";
+
+function AiIntegration({ sections, section, mutate }) {
+  const apiKeyRef = useRef(null);
+  const theme = useColorTheme();
+  const { sessionToken } = useUser();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    try {
+      await sendApiRequest(sessionToken, "POST", "ai", {
+        type: section.id,
+        token: apiKeyRef?.current.value,
+      });
+      await mutate();
+      Toast.show({ type: "success" });
+    } catch {
+      apiKeyRef.current.focus();
+      Toast.show({ type: "error" });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <View
+      style={{
+        backgroundColor: theme[3],
+        padding: 20,
+        borderRadius: 10,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+        borderTopWidth: 2,
+        gap: 10,
+        borderTopColor: theme[4],
+        flexDirection: "row",
+      }}
+    >
+      <TextField
+        editable={!isLoading}
+        onSubmitEditing={handleSubmit}
+        variant="filled+outlined"
+        placeholder="API key"
+        style={{ flex: 1 }}
+        inputRef={apiKeyRef}
+      />
+      <Button
+        onPress={handleSubmit}
+        isLoading={isLoading}
+        variant="filled"
+        iconPosition="end"
+        backgroundColors={{
+          default: theme[5],
+          hovered: theme[6],
+          pressed: theme[7],
+        }}
+        text="Continue"
+        icon="east"
+        style={{ width: 130 }}
+      />
+    </View>
+  );
+}
 
 export default function Page() {
   const theme = useColorTheme();
+  const [activeSections, setActiveSections] = useState([]);
 
-  return (
-    <ImageBackground
-      style={{
-        padding: 20,
-        borderRadius: 50,
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        marginVertical: 50,
-        marginBottom: 20,
-        overflow: "hidden",
-        backgroundColor: theme[3],
-      }}
-      source={{
-        uri: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' viewBox='0 0 700 700' width='700' height='700'%3E%3Cdefs%3E%3ClinearGradient gradientTransform='rotate(-150, 0.5, 0.5)' x1='50%25' y1='0%25' x2='50%25' y2='100%25' id='gggrain-gradient2'%3E%3Cstop stop-color='hsl(194, 83%25, 49%25)' stop-opacity='1' offset='-0%25'%3E%3C/stop%3E%3Cstop stop-color='rgba(255,255,255,0)' stop-opacity='0' offset='100%25'%3E%3C/stop%3E%3C/linearGradient%3E%3ClinearGradient gradientTransform='rotate(150, 0.5, 0.5)' x1='50%25' y1='0%25' x2='50%25' y2='100%25' id='gggrain-gradient3'%3E%3Cstop stop-color='hsl(0, 100%25, 60%25)' stop-opacity='1'%3E%3C/stop%3E%3Cstop stop-color='rgba(255,255,255,0)' stop-opacity='0' offset='100%25'%3E%3C/stop%3E%3C/linearGradient%3E%3Cfilter id='gggrain-filter' x='-20%25' y='-20%25' width='140%25' height='140%25' filterUnits='objectBoundingBox' primitiveUnits='userSpaceOnUse' color-interpolation-filters='sRGB'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.55' numOctaves='2' seed='2' stitchTiles='stitch' x='0%25' y='0%25' width='100%25' height='100%25' result='turbulence'%3E%3C/feTurbulence%3E%3CfeColorMatrix type='saturate' values='0' x='0%25' y='0%25' width='100%25' height='100%25' in='turbulence' result='colormatrix'%3E%3C/feColorMatrix%3E%3CfeComponentTransfer x='0%25' y='0%25' width='100%25' height='100%25' in='colormatrix' result='componentTransfer'%3E%3CfeFuncR type='linear' slope='3'%3E%3C/feFuncR%3E%3CfeFuncG type='linear' slope='3'%3E%3C/feFuncG%3E%3CfeFuncB type='linear' slope='3'%3E%3C/feFuncB%3E%3C/feComponentTransfer%3E%3CfeColorMatrix x='0%25' y='0%25' width='100%25' height='100%25' in='componentTransfer' result='colormatrix2' type='matrix' values='1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 19 -11'%3E%3C/feColorMatrix%3E%3C/filter%3E%3C/defs%3E%3Cg%3E%3Crect width='100%25' height='100%25' fill='hsl(22, 100%25, 60%25)'%3E%3C/rect%3E%3Crect width='100%25' height='100%25' fill='url(%23gggrain-gradient3)'%3E%3C/rect%3E%3Crect width='100%25' height='100%25' fill='url(%23gggrain-gradient2)'%3E%3C/rect%3E%3Crect width='100%25' height='100%25' fill='transparent' filter='url(%23gggrain-filter)' opacity='1' style='mix-blend-mode: soft-light'%3E%3C/rect%3E%3C/g%3E%3C/svg%3E",
-      }}
-    >
-      <View
+  const { data, error } = useSWR(["ai"]);
+
+  if (process.env.NODE_ENV === "production")
+    return (
+      <ImageBackground
         style={{
-          flexDirection: "column",
+          padding: 20,
+          borderRadius: 50,
+          flex: 1,
           alignItems: "center",
-          gap: 5,
+          justifyContent: "center",
+          marginVertical: 50,
+          marginBottom: 20,
+          overflow: "hidden",
+          backgroundColor: theme[3],
+        }}
+        source={{
+          uri: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' viewBox='0 0 700 700' width='700' height='700'%3E%3Cdefs%3E%3ClinearGradient gradientTransform='rotate(-150, 0.5, 0.5)' x1='50%25' y1='0%25' x2='50%25' y2='100%25' id='gggrain-gradient2'%3E%3Cstop stop-color='hsl(194, 83%25, 49%25)' stop-opacity='1' offset='-0%25'%3E%3C/stop%3E%3Cstop stop-color='rgba(255,255,255,0)' stop-opacity='0' offset='100%25'%3E%3C/stop%3E%3C/linearGradient%3E%3ClinearGradient gradientTransform='rotate(150, 0.5, 0.5)' x1='50%25' y1='0%25' x2='50%25' y2='100%25' id='gggrain-gradient3'%3E%3Cstop stop-color='hsl(0, 100%25, 60%25)' stop-opacity='1'%3E%3C/stop%3E%3Cstop stop-color='rgba(255,255,255,0)' stop-opacity='0' offset='100%25'%3E%3C/stop%3E%3C/linearGradient%3E%3Cfilter id='gggrain-filter' x='-20%25' y='-20%25' width='140%25' height='140%25' filterUnits='objectBoundingBox' primitiveUnits='userSpaceOnUse' color-interpolation-filters='sRGB'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.55' numOctaves='2' seed='2' stitchTiles='stitch' x='0%25' y='0%25' width='100%25' height='100%25' result='turbulence'%3E%3C/feTurbulence%3E%3CfeColorMatrix type='saturate' values='0' x='0%25' y='0%25' width='100%25' height='100%25' in='turbulence' result='colormatrix'%3E%3C/feColorMatrix%3E%3CfeComponentTransfer x='0%25' y='0%25' width='100%25' height='100%25' in='colormatrix' result='componentTransfer'%3E%3CfeFuncR type='linear' slope='3'%3E%3C/feFuncR%3E%3CfeFuncG type='linear' slope='3'%3E%3C/feFuncG%3E%3CfeFuncB type='linear' slope='3'%3E%3C/feFuncB%3E%3C/feComponentTransfer%3E%3CfeColorMatrix x='0%25' y='0%25' width='100%25' height='100%25' in='componentTransfer' result='colormatrix2' type='matrix' values='1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 19 -11'%3E%3C/feColorMatrix%3E%3C/filter%3E%3C/defs%3E%3Cg%3E%3Crect width='100%25' height='100%25' fill='hsl(22, 100%25, 60%25)'%3E%3C/rect%3E%3Crect width='100%25' height='100%25' fill='url(%23gggrain-gradient3)'%3E%3C/rect%3E%3Crect width='100%25' height='100%25' fill='url(%23gggrain-gradient2)'%3E%3C/rect%3E%3Crect width='100%25' height='100%25' fill='transparent' filter='url(%23gggrain-filter)' opacity='1' style='mix-blend-mode: soft-light'%3E%3C/rect%3E%3C/g%3E%3C/svg%3E",
         }}
       >
-        <Icon size={70} style={{ color: theme[12] }}>
-          raven
-        </Icon>
-        <Text
-          weight={300}
+        <View
           style={{
-            color: theme[12],
-            fontSize: 60,
+            flexDirection: "column",
+            alignItems: "center",
+            gap: 5,
           }}
         >
-          sidekick
+          <Icon size={70} style={{ color: theme[12] }}>
+            raven
+          </Icon>
+          <Text
+            weight={300}
+            style={{
+              color: theme[12],
+              fontSize: 60,
+            }}
+          >
+            sidekick
+          </Text>
+        </View>
+        <Text
+          weight={900}
+          style={{ opacity: 0.6, fontSize: 20, marginTop: 5, color: theme[12] }}
+        >
+          arriving 2025
         </Text>
-      </View>
-      <Text
-        weight={900}
-        style={{ opacity: 0.6, fontSize: 20, marginTop: 5, color: theme[12] }}
-      >
-        arriving 2025
+      </ImageBackground>
+    );
+
+  const sections = [
+    {
+      id: "GEMINI",
+      header: {
+        icon: "https://assets.dysperse.com/google.svg?t",
+        primary: "Gemini 1.5",
+        secondary: "by Google",
+      },
+    },
+    {
+      id: "OPENAI",
+      header: {
+        icon: "https://cdn.brandfetch.io/idR3duQxYl/theme/dark/symbol.svg?c=1dxbfHSJFAPEGdCLU4o5B",
+        primary: "GPT 4.0",
+        secondary: "by OpenAI",
+      },
+    },
+  ];
+
+  return (
+    <SettingsScrollView>
+      <Text style={settingStyles.title}>Sidekick</Text>
+      <Text style={{ marginBottom: 20 }}>
+        Let Sidekick do the heavy lifting for you. Connect your AI accounts with
+        Dysperse to get started.
       </Text>
-    </ImageBackground>
+
+      {error && <ErrorAlert style={{ marginBottom: 20 }} />}
+
+      <Accordion
+        activeSections={activeSections}
+        align="bottom"
+        sections={sections}
+        sectionContainerStyle={{ backgroundColor: "transparent" }}
+        containerStyle={{ gap: 10 }}
+        renderHeader={(section) => (
+          <ListItemButton
+            variant="filled"
+            style={{
+              borderBottomLeftRadius: activeSections.includes(
+                sections.indexOf(section)
+              )
+                ? 0
+                : 10,
+              borderBottomRightRadius: activeSections.includes(
+                sections.indexOf(section)
+              )
+                ? 0
+                : 10,
+            }}
+            onPress={() => setActiveSections([sections.indexOf(section)])}
+          >
+            <Image
+              source={{ uri: section.header.icon }}
+              style={{ width: 30, height: 30 }}
+            />
+            <ListItemText
+              primary={section.header.primary}
+              secondary={section.header.secondary}
+            />
+          </ListItemButton>
+        )}
+        renderContent={(t) => <AiIntegration sections={sections} section={t} />}
+        onChange={setActiveSections}
+      />
+    </SettingsScrollView>
   );
 }
+
--- ui/Error/index.tsx
+++ ui/Error/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { StyleProp, View, ViewStyle } from "react-native";
 import Icon from "../Icon";
 import Text from "../Text";
 import { useColor } from "../color";
@@ -6,27 +6,33 @@ import { useColor } from "../color";
 export default function ErrorAlert({
   message = "Something went wrong. Please try again later.",
   icon = "error",
+  style,
 }: {
   message?: string;
   icon?: string;
+  style?: StyleProp<ViewStyle>;
 }) {
   const red = useColor("red");
   return (
     <View
-      style={{
-        gap: 15,
-        backgroundColor: red[3],
-        borderColor: red[5],
-        flexDirection: "row",
-        borderRadius: 20,
-        alignItems: "center",
-        borderWidth: 2,
-        paddingHorizontal: 20,
-        paddingVertical: 15,
-      }}
+      style={[
+        {
+          gap: 15,
+          backgroundColor: red[3],
+          borderColor: red[5],
+          flexDirection: "row",
+          borderRadius: 20,
+          alignItems: "center",
+          borderWidth: 2,
+          paddingHorizontal: 20,
+          paddingVertical: 15,
+        },
+        style,
+      ]}
     >
       <Icon style={{ color: red[11] }}>{icon}</Icon>
       <Text style={{ color: red[11], lineHeight: 23, flex: 1 }}>{message}</Text>
     </View>
   );
 }
+
