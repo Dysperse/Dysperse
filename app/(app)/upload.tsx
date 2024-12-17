96ec85e5b293d8cdd3ff77324af5475ca18ae4e8
Manu G <manusvathgurudath@gmail.com>
2024-12-17 09:26:18 -0800

    adsf

--- app/(app)/upload.tsx
+++ app/(app)/upload.tsx
@@ -2,12 +2,122 @@ import ContentWrapper from "@/components/layout/content";
 import { Avatar } from "@/ui/Avatar";
 import { Button } from "@/ui/Button";
 import { useColorTheme } from "@/ui/color/theme-provider";
+import ErrorAlert from "@/ui/Error";
+import Icon from "@/ui/Icon";
+import IconButton from "@/ui/IconButton";
+import { ListItemButton } from "@/ui/ListItemButton";
+import ListItemText from "@/ui/ListItemText";
+import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
 import TextField from "@/ui/TextArea";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import useSWR from "swr";
+
+const PasteInput = ({ input, setInput, handleContinue }) => {
+  return (
+    <>
+      <TextField
+        variant="filled+outlined"
+        multiline
+        value={input}
+        onChangeText={setInput}
+        style={{
+          flex: 1,
+          fontFamily: "mono",
+          paddingHorizontal: 20,
+          paddingVertical: 20,
+        }}
+        placeholder="Paste anything here..."
+      />
+      <Button
+        large
+        bold
+        disabled={!input.trim()}
+        text="Continue"
+        iconPosition="end"
+        onPress={handleContinue}
+        icon="east"
+        variant="filled"
+        containerStyle={{ marginTop: 10, opacity: input.trim() ? 1 : 0.5 }}
+      />
+    </>
+  );
+};
+
+const AITasks = ({ input, setSlide }) => {
+  const theme = useColorTheme();
+  const { data, error } = useSWR([
+    "ai/import-tasks",
+    {},
+    process.env.EXPO_PUBLIC_API_URL,
+    {
+      method: "POST",
+      body: JSON.stringify({ text: input }),
+    },
+  ]);
+  return (
+    <View style={{ flex: 1 }}>
+      {data ? (
+        <>
+          <LinearGradient
+            colors={[theme[1], "transparent"]}
+            style={{
+              height: 60,
+              marginBottom: -60,
+              zIndex: 1,
+              marginTop: -20,
+              pointerEvents: "none",
+            }}
+          />
+          <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
+            {data.map((task, i) => (
+              <ListItemButton key={i}>
+                <Icon>circle</Icon>
+                <ListItemText
+                  primary={task.name}
+                  secondary={task.description}
+                />
+              </ListItemButton>
+            ))}
+          </ScrollView>
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 10,
+            }}
+          >
+            <IconButton
+              icon="west"
+              size={50}
+              onPress={() => setSlide(0)}
+              variant="outlined"
+            />
+            <Button
+              text="Add tasks"
+              icon="add"
+              iconPosition="start"
+              variant="filled"
+              large
+              bold
+              containerStyle={{ flex: 1 }}
+            />
+          </View>
+        </>
+      ) : error ? (
+        <ErrorAlert />
+      ) : (
+        <Spinner />
+      )}
+    </View>
+  );
+};
 
 export default function Page() {
-  const theme = useColorTheme();
+  const [input, setInput] = useState("");
+  const [slide, setSlide] = useState(0);
 
   return (
     <ContentWrapper>
@@ -44,26 +154,15 @@ export default function Page() {
           </Text>
         </View>
 
-        <TextField
-          variant="filled+outlined"
-          multiline
-          style={{
-            flex: 1,
-            fontFamily: "mono",
-            paddingHorizontal: 20,
-            paddingVertical: 20,
-          }}
-          placeholder="Paste anything here..."
-        />
-        <Button
-          large
-          bold
-          text="Continue"
-          iconPosition="end"
-          icon="east"
-          variant="filled"
-          containerStyle={{ marginTop: 10 }}
-        />
+        {slide === 0 ? (
+          <PasteInput
+            handleContinue={() => setSlide(1)}
+            input={input}
+            setInput={setInput}
+          />
+        ) : (
+          <AITasks input={input} setSlide={setSlide} />
+        )}
       </View>
     </ContentWrapper>
   );
