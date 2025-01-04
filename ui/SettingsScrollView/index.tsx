01b016b02844026da8a1f494b4ad32e052e33797
Manu G <manusvathgurudath@gmail.com>
2025-01-03 18:55:10 -0800

    asdf

--- ui/SettingsScrollView/index.tsx
+++ ui/SettingsScrollView/index.tsx
@@ -1,21 +1,18 @@
 import { useResponsiveBreakpoints } from "@/helpers/useResponsiveBreakpoints";
-import { ScrollViewProps, useWindowDimensions } from "react-native";
+import { ScrollViewProps } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function SettingsScrollView(
   props: ScrollViewProps & { hideBack?: boolean }
 ) {
   const breakpoints = useResponsiveBreakpoints();
-  const { height } = useWindowDimensions();
-  const insets = useSafeAreaInsets();
 
   return (
     <ScrollView
       {...props}
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{
-        marginTop: 100,
+        marginTop: breakpoints.md ? 0 : 100,
         paddingVertical: props.hideBack ? 0 : 50,
         paddingTop: breakpoints.md ? 50 : 20,
         paddingHorizontal: props.hideBack ? 0 : 30,
