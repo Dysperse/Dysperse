214727aeaec00af5c964710154506782cbed3cb8
Manu G <manusvathgurudath@gmail.com>
2025-01-20 23:50:40 -0800

    a

--- components/collections/views/map/index.tsx
+++ components/collections/views/map/index.tsx
@@ -1,4 +1,5 @@
 import { useColorTheme } from "@/ui/color/theme-provider";
+import Text from "@/ui/Text";
 import { Platform, View } from "react-native";
 import DomMapView from "./map";
 import NativeMapView from "./NativeMap";
@@ -8,7 +9,15 @@ export default function MapView() {
 
   return (
     <View style={{ flex: 1, flexDirection: "row", padding: 20, paddingTop: 0 }}>
-      <View style={{ flex: 1 }}></View>
+      <View style={{ flex: 1, padding: 20, justifyContent: "center" }}>
+        <Text style={{ fontFamily: "serifText800", fontSize: 37 }}>
+          Coming soon!
+        </Text>
+        <Text style={{ opacity: 0.8, marginTop: 10, fontSize: 17 }}>
+          Soon, you'll be able to view your {"\n"}collection on a map. Stay
+          tuned!
+        </Text>
+      </View>
       <View
         style={{
           flex: 2,
--- components/collections/views/map/map.tsx
+++ components/collections/views/map/map.tsx
@@ -1,11 +1,45 @@
 "use dom";
+import { useDarkMode } from "@/ui/color";
+import { useColorTheme } from "@/ui/color/theme-provider";
 import { Map, Marker } from "pigeon-maps";
 import React from "react";
 
 export default function DomMapView() {
+  const isDark = useDarkMode();
+  const theme = useColorTheme();
+
   return (
-    <Map height={800} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
-      <Marker width={50} anchor={[50.879, 4.6997]} />
-    </Map>
+    <div
+      className="t"
+      style={{
+        height: "100%",
+      }}
+    >
+      {isDark && (
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `.leaflet-container {
+                background: ${theme[3]};
+            }
+            .t{
+                filter: invert(100%) hue-rotate(180deg) brightness(95%) contrast(90%);
+            }`,
+          }}
+        />
+      )}
+      <Map
+        height={"100%" as any}
+        defaultCenter={[50.879, 4.6997]}
+        defaultZoom={11}
+        attribution={<>Leaflet</>}
+        twoFingerDrag
+        provider={(x, y, z) => {
+          const s = String.fromCharCode(97 + ((x + y + z) % 3));
+          return `https://${s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/${z}/${x}/${y}.png`;
+        }}
+      >
+        <Marker width={50} anchor={[50.879, 4.6997]} />
+      </Map>
+    </div>
   );
 }
