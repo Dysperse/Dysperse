1533aa3079f7b57ce237730eb07c77afdd79cab2
Manu G <manusvathgurudath@gmail.com>
2025-01-23 23:26:24 -0800

    W

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,53 @@
 import { withSentry } from "@sentry/react-native/expo";
-import themes from "components/themes.json";
 import { ConfigContext, ExpoConfig } from "expo/config";
-import * as themeColors from "themes";
-import { hslToHex } from "./helpers/hslToHex";
+import * as themeColors from "./themes.js";
+
+export function hslToHex(h, s, l) {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+  };
+  return `${f(0)}${f(8)}${f(4)}`;
+}
+
+const themes = [
+  "tomato",
+  "red",
+  "crimson",
+  "ruby",
+  "jade",
+  "iris",
+  "pink",
+  "plum",
+  "purple",
+  "violet",
+  "indigo",
+  "blue",
+  "cyan",
+  "teal",
+  "green",
+  "grass",
+  "orange",
+  "brown",
+  "sky",
+  "mint",
+  "lime",
+  "yellow",
+  "amber",
+  "gray",
+  "mauve",
+  "slate",
+  "sage",
+  "olive",
+  "sand",
+  "bronze",
+  "gold",
+];
 
 const IS_DEV = process.env.APP_VARIANT === "development";
 
@@ -81,8 +126,8 @@ export default ({ config }: ConfigContext): ExpoConfig =>
         [
           "expo-alternate-app-icons",
           [
-            ...Object.entries(themes).map(([key, value]) => ({
-              name: `${value.name}Dark`,
+            ...themes.map((key) => ({
+              name: `${key}Dark`,
               ios: `./assets/icons/${key}Dark.png`,
               android: {
                 foregroundImage: `./assets/icons/${key}Dark.png`,
@@ -98,8 +143,8 @@ export default ({ config }: ConfigContext): ExpoConfig =>
               },
             })),
 
-            ...Object.entries(themes).map(([key, value]) => ({
-              name: `${value.name}Light`,
+            ...themes.map((key) => ({
+              name: `${key}Light`,
               ios: `./assets/icons/${key}Light.png`,
               android: {
                 foregroundImage: `./assets/icons/${key}Light.png`,
