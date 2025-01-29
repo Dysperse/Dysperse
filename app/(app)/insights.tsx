77caeacaa2c3791b049683e4407f7fd292a3c9cd
Manu G <manusvathgurudath@gmail.com>
2025-01-28 23:18:29 -0800

    W

--- app/(app)/insights.tsx
+++ app/(app)/insights.tsx
@@ -1,60 +1,74 @@
-import { Co2 } from "@/components/insights/Co2";
-import { Header } from "@/components/insights/Header";
-import { Heatmap } from "@/components/insights/Heatmap";
-import { MemberSince } from "@/components/insights/MemberSince";
-import { TasksCreated } from "@/components/insights/TasksCreated";
+import { Button } from "@/ui/Button";
 import ErrorAlert from "@/ui/Error";
+import MenuPopover from "@/ui/MenuPopover";
 import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
-import { useRef } from "react";
+import { useState } from "react";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import useSWR from "swr";
+import { MenuButton } from "./home";
+
+function YearSelector({ years, year, setYear }) {
+  return (
+    <MenuPopover
+      trigger={<Button style={{ marginTop: 20 }} />}
+      options={years.map((y) => ({
+        text: y.toString(),
+        selected: y === year,
+        callback: () => setYear(y),
+      }))}
+    />
+  );
+}
+
+function Insights({ year }) {
+  const { data, error } = useSWR(["user/insights", { year }]);
+
+  return data ? (
+    <Text>{JSON.stringify(data, null, 2)}</Text>
+  ) : (
+    <View>{error ? <ErrorAlert /> : <Spinner />}</View>
+  );
+}
 
 export default function Page() {
-  const { data, error } = useSWR(["user/insights"]);
-  const ref = useRef<ScrollView>(null);
+  const { data, error } = useSWR(["user/insights/years"]);
+  const [year, setYear] = useState(new Date().getFullYear());
 
-  return (
-    <>
-      <Header scrollRef={ref} />
-      {data ? (
-        <View style={{ flex: 1 }}>
-          <ScrollView
-            contentContainerStyle={{
-              padding: 20,
-              paddingTop: 50,
-            }}
-            ref={ref}
-          >
-            <View style={{ marginVertical: 50 }}>
-              <Text
-                style={{
-                  fontSize: 40,
-                  marginBottom: 10,
-                  textAlign: "center",
-                  fontFamily: "serifText800",
-                }}
-              >
-                Insights
-              </Text>
-            </View>
-            <View style={{ gap: 10 }}>
-              <Co2 data={data} />
-              <TasksCreated data={data} />
-              <MemberSince />
-              <Heatmap data={data} />
-            </View>
-          </ScrollView>
-        </View>
-      ) : (
-        <View
-          style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
-        >
-          {error ? <ErrorAlert /> : <Spinner />}
-        </View>
+  return data ? (
+    <ScrollView contentContainerStyle={{ paddingVertical: 50 }}>
+      <MenuButton gradient addInsets back />
+      <Text
+        style={{
+          fontFamily: "serifText700",
+          fontSize: 30,
+          textAlign: "center",
+          marginTop: 50,
+        }}
+      >
+        Insights
+      </Text>
+      {data.years.length > 0 && (
+        <YearSelector years={data.years} year={year} setYear={setYear} />
       )}
-    </>
+
+      <Insights year={year} />
+    </ScrollView>
+  ) : (
+    <View style={{ height: "100%", width: "100%" }}>
+      <MenuButton gradient addInsets back />
+      <View
+        style={{
+          height: "100%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {error ? <ErrorAlert /> : <Spinner />}
+      </View>
+    </View>
   );
 }
 
