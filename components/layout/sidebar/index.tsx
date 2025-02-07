cb7075f4211e45d19b20d7fa4d30e342ed403930
Manu G <manusvathgurudath@gmail.com>
2025-02-07 22:44:03 +0000

    W

--- components/layout/sidebar/index.tsx
+++ components/layout/sidebar/index.tsx
@@ -519,9 +519,13 @@ const TimeZoneModal = () => {
         onPress={() => ref.current.present()}
         icon="travel"
         height={45}
-        text={`${hours} hour${Math.abs(hours) !== 1 ? "s" : ""} ${
-          hours < 0 ? "ahead" : "behind"
-        }`}
+        text={
+          hours === 0
+            ? "Travel mode"
+            : `${hours} hour${Math.abs(hours) !== 1 ? "s" : ""} ${
+                hours < 0 ? "ahead" : "behind"
+              }`
+        }
         bold
         textProps={{ weight: 700 }}
       />
