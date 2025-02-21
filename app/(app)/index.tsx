988780c22d7b721c09c31c86d1c126dd38ad0af1
Manu G <manusvathgurudath@gmail.com>
2025-02-21 15:44:38 -0800

    W

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,9 +1,14 @@
+import Content from "@/components/layout/content";
+import Spinner from "@/ui/Spinner";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Page() {
+  const [hasSet, setHasSet] = useState(false);
+
   const setCurrentPage = useCallback(async () => {
+    setHasSet(true);
     const lastViewedRoute = await AsyncStorage.getItem("lastViewedRoute");
     router.replace(
       lastViewedRoute
@@ -15,7 +20,16 @@ export default function Page() {
   }, []);
 
   useEffect(() => {
-    setCurrentPage();
-  }, []);
-  return null;
+    if (!hasSet) setCurrentPage();
+    else router.replace("/home");
+  }, [hasSet, setCurrentPage]);
+
+  return (
+    <Content
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <Spinner />
+    </Content>
+  );
 }
+
