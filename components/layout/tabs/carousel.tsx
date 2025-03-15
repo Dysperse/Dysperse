0fa335ab742fcfbc88535b62da0b6414e34fd1ac
Manu G <manusvathgurudath@gmail.com>
2025-03-14 19:29:50 -0700

    W

--- components/layout/tabs/carousel.tsx
+++ components/layout/tabs/carousel.tsx
@@ -312,7 +312,10 @@ function OpenTabsList() {
         pressed: theme[4],
       }}
       height={50}
-      iconStyle={{ marginLeft: -2, marginRight: breakpoints.md ? -3 : 1 }}
+      iconStyle={{
+        marginLeft: breakpoints.md ? -2 : 1,
+        marginRight: breakpoints.md ? -3 : -1,
+      }}
       containerStyle={{
         borderRadius: 15,
       }}
