eaef7b67c95949b69e3e8bef80d0c03b87c6d10e
Manu G <manusvathgurudath@gmail.com>
2025-06-03 19:58:11 -0700

    fix: ensure task completion status returns boolean

--- helpers/getTaskCompletionStatus.tsx
+++ helpers/getTaskCompletionStatus.tsx
@@ -2,14 +2,12 @@ import dayjs from "dayjs";
 
 export const getTaskCompletionStatus = (task, iteration): boolean => {
   if (task.recurrenceRule) {
-    return (
-      iteration &&
-      task.completionInstances.find(
-        (instance) =>
-          dayjs(instance.iteration || instance.recurrenceDay).isValid() &&
-          dayjs(instance.iteration || instance.recurrenceDay).toISOString() ===
-            dayjs(iteration.toString()).toISOString()
-      )
+    if (!iteration) return false;
+    return task.completionInstances.some(
+      (instance) =>
+        dayjs(instance.iteration || instance.recurrenceDay).isValid() &&
+        dayjs(instance.iteration || instance.recurrenceDay).toISOString() ===
+          dayjs(iteration.toString()).toISOString()
     );
   } else {
     return task.completionInstances.length > 0;
