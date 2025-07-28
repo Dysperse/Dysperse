57724ff22235b30f5d90ebbf273091d5187377b8
Manu G <manusvathgurudath@gmail.com>
2025-07-27 19:15:03 -0700

    W

--- app/r/[id]/index.tsx
+++ app/r/[id]/index.tsx
@@ -1,12 +1,22 @@
+import Spinner from "@/ui/Spinner";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Redirect, useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { useEffect } from "react";
+import { View } from "react-native";
 
 export default function Page() {
   const { id } = useLocalSearchParams();
+
   useEffect(() => {
-    AsyncStorage.setItem("referredBy", id as string);
+    (async () => {
+      await AsyncStorage.setItem("referredBy", id as string);
+      router.replace("/auth");
+    })();
   }, [id]);
 
-  return <Redirect href={`/auth`} />;
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Spinner />
+    </View>
+  );
 }
