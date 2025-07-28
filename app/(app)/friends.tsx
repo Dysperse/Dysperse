6b7262c9a6e36639e800f657574406a918953050
Manu G <manusvathgurudath@gmail.com>
2025-07-27 17:37:56 -0700

    asdf

--- app/(app)/friends.tsx
+++ app/(app)/friends.tsx
@@ -8,9 +8,13 @@ import Icon from "@/ui/Icon";
 import IconButton from "@/ui/IconButton";
 import { ListItemButton } from "@/ui/ListItemButton";
 import ListItemText from "@/ui/ListItemText";
+import RefreshControl from "@/ui/RefreshControl";
 import Spinner from "@/ui/Spinner";
 import Text from "@/ui/Text";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import dayjs from "dayjs";
 import * as Contacts from "expo-contacts";
+import { impactAsync, ImpactFeedbackStyle } from "expo-haptics";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
@@ -20,8 +24,9 @@ import { MenuButton } from "./home";
 function FriendsList() {
   const { session } = useSession();
   const [contacts, setContacts] = useState([]);
-  console.log(session);
-  const { data, error } = useSWR("user/friends", {
+  const [hasContactsPermission, setHasContactsPermission] = useState(false);
+
+  const { data, error, mutate } = useSWR("user/friends", {
     fetcher: () =>
       fetch(`${process.env.EXPO_PUBLIC_API_URL}/user/friends/suggestions`, {
         method: "POST",
@@ -35,8 +40,6 @@ function FriendsList() {
         },
       }).then((res) => res.json()),
   });
-
-  const [hasContactsPermission, setHasContactsPermission] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -64,33 +67,101 @@ function FriendsList() {
     })();
   }, [hasContactsPermission]);
 
+  useEffect(() => {
+    if (hasContactsPermission || contacts.length > 0) {
+      mutate();
+    }
+  }, [hasContactsPermission, contacts]);
+
+  const acceptedFriends = data?.friends.filter((t) => t.accepted) || [];
+  const pendingFriends = data?.friends.filter((t) => !t.accepted) || [];
+
+  const inContacts = data
+    ? contacts
+        .filter(
+          (t) =>
+            t.name &&
+            ((t.emails && t.emails.length > 0) ||
+              (t.phoneNumbers && t.phoneNumbers.length > 0))
+        )
+        .filter(
+          (t) =>
+            data.contactsUsingDysperse.some(
+              (c) => c.profile.email === t.emails?.[0]?.email
+            ) || data.friends.some((c) => c.user.email === t.emails?.[0]?.email)
+        )
+        .map((contact) => ({
+          contactImage: contact.imageAvailable ? contact.image.uri : undefined,
+          suggestion: true,
+          profile: {
+            name: contact.name,
+            secondaryText:
+              contact.phoneNumbers?.[0]?.number || contact.emails?.[0]?.email,
+            email: contact.emails?.[0]?.email,
+          },
+        }))
+    : [];
+
   return data ? (
     <View style={{ flex: 1 }}>
       <FlatList
+        refreshControl={
+          <RefreshControl refreshing={false} onRefresh={() => mutate()} />
+        }
         data={[
-          ...data.friends,
+          pendingFriends.length > 0 && "Pending",
+          ...pendingFriends,
+          acceptedFriends.length > 0 && "Your friends",
+          ...acceptedFriends,
+          data.contactsUsingDysperse.length > 0 && "Contacts on Dysperse",
           ...data.contactsUsingDysperse.map((t) => ({
             ...t,
             suggestion: true,
           })),
-        ]}
-        renderItem={({ item }) => (
-          <ListItemButton>
-            <Avatar
-              image={item.user?.profile?.picture || item.profile?.contactImage}
-              name={item.user?.profile?.name || item.profile?.name}
-              size={50}
-            />
-            <ListItemText
-              primary={item.user?.profile?.name || item.profile?.name}
-              secondary={
-                item.suggestion
-                  ? "In your contacts • Uses Dysperse"
-                  : item.user?.profile?.email
-              }
-            />
-          </ListItemButton>
-        )}
+          inContacts.length > 0 && "In your contacts",
+          ...inContacts,
+        ].filter(Boolean)}
+        contentContainerStyle={{
+          paddingHorizontal: 10,
+          paddingTop: 10,
+          paddingBottom: 70,
+        }}
+        renderItem={({ item }) =>
+          typeof item === "string" ? (
+            <Text
+              variant="eyebrow"
+              style={{ paddingHorizontal: 15, paddingTop: 20 }}
+            >
+              {item}
+            </Text>
+          ) : (
+            <ListItemButton>
+              <Avatar
+                disabled
+                image={item.user?.profile?.picture || item?.contactImage}
+                name={item.user?.profile?.name || item.profile?.name}
+                size={50}
+              />
+              <ListItemText
+                primary={item.user?.profile?.name || item.profile?.name}
+                secondary={
+                  item.profile?.lastActive || item.user?.profile?.lastActive
+                    ? `Active ${dayjs(
+                        item.profile?.lastActive ||
+                          item.user?.profile?.lastActive
+                      ).fromNow()}`
+                    : item.user?.profile?.email || item.profile?.secondaryText
+                }
+              />
+              {item.suggestion && (
+                <Button
+                  text={item.profile?.lastActive ? "Add" : "Invite"}
+                  variant={item.profile?.lastActive ? "filled" : "outlined"}
+                />
+              )}
+            </ListItemButton>
+          )
+        }
       />
     </View>
   ) : (
@@ -153,6 +224,61 @@ function ContactBanner() {
   );
 }
 
+function AddFriendsPromo() {
+  const theme = useColorTheme();
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const value = await AsyncStorage.getItem("addFriendsPromo");
+      if (value === "false") {
+        setShow(false);
+      }
+    })();
+  }, []);
+
+  return (
+    show && (
+      <View
+        style={{
+          marginTop: 10,
+          backgroundColor: theme[3],
+          borderRadius: 20,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          gap: 20,
+          padding: 20,
+          paddingHorizontal: 25,
+        }}
+      >
+        <Icon bold size={30}>
+          waving_hand
+        </Icon>
+        <View style={{ flex: 1 }}>
+          <Text style={{ color: theme[11] }} weight={800}>
+            Invite a friend—get 25 storage credits each!
+          </Text>
+        </View>
+        <IconButton
+          onPress={() => {
+            setShow(false);
+            impactAsync(ImpactFeedbackStyle.Light);
+            AsyncStorage.setItem("addFriendsPromo", "false");
+          }}
+          icon="close"
+          style={{ marginRight: -5 }}
+          backgroundColors={{
+            default: theme[4],
+            hovered: theme[5],
+            pressed: theme[6],
+          }}
+        />
+      </View>
+    )
+  );
+}
+
 export default function Page() {
   const theme = useColorTheme();
 
@@ -194,38 +320,7 @@ export default function Page() {
           />
         </View>
 
-        <View
-          style={{
-            marginTop: 10,
-            backgroundColor: theme[3],
-            borderRadius: 20,
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            gap: 20,
-            padding: 20,
-            paddingHorizontal: 25,
-          }}
-        >
-          <Icon bold size={30}>
-            waving_hand
-          </Icon>
-          <View style={{ flex: 1 }}>
-            <Text style={{ color: theme[11] }} weight={800}>
-              Invite a friend—get 25 storage credits each!
-            </Text>
-          </View>
-          <IconButton
-            icon="close"
-            style={{ marginRight: -5 }}
-            onPress={() => {}}
-            backgroundColors={{
-              default: theme[4],
-              hovered: theme[5],
-              pressed: theme[6],
-            }}
-          />
-        </View>
+        <AddFriendsPromo />
         <ContactBanner />
       </View>
 
